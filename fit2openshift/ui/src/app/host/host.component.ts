781965f4da09ffa19b1ddbd3ae729cf8a04bb2bd
shenchenyang <scydeai@qq.com>
2019-02-13 18:50:18 +0800

    add chars

---
+++ fit2openshift/ui/src/app/host/host.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-host',
+  templateUrl: './host.component.html',
+  styleUrls: ['./host.component.css']
+})
+export class HostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ fit2openshift/ui/src/app/host/host.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HostComponent } from './host.component';
+
+@NgModule({
+  declarations: [HostComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HostModule { }
