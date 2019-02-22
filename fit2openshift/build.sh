76ac756d9c947d6e17d69b63130d3a4e5d90a5e8
chenyang <chenyang@fit2cloud.com>
2019-02-22 17:43:27 +0800

    install

---
+++ fit2openshift/build.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+
+red=31
+green=32
+yellow=33
+blue=34
+
+function colorMsg()
+{
+  echo -e "\033[$1m $2 \033[0m"
+}
+
+colorMsg $blue "Build FIT2OPENSHIFT IMAGES"
+logPath="/opt/fit2openshift/logs/install/"
+timestamp=$(date -d now +%F)
+errorLogFile=${logPath}"error/install_error_"${timestamp}".log"
+infoLogFile=${logPath}"info/install_info_"${timestamp}".log"
+fullLogFile=${logPath}"install_"${timestamp}".log"
+printf "%-65s .......... " "Build fit2openshift webconsole ui:"
+cd ui && ng config -g cli.warnings.versionMismatch false && ng build 1>>$infoLogFile 2>>$errorLogFile >>$fullLogFile 1>&2
+if [ "$?" == "0" ];then
+    colorMsg $green "[OK]"
+else
+    colorMsg $red "[DEFEATED]"
+    printf "\n"
+    printf "Build fit2openshift webconsole ui  defeated! An error log in :"${errorLogFile}
+    printf "\n"
+    exit 1
+fi
+
+printf "%-65s .......... " "Build fit2openshift webconsole api: "
+cd .. && docker build --rm=true --tag=registry.fit2cloud.com/fit2anything/fit2openshift/fit2openshift-app:test . 1>>$infoLogFile 2>>$errorLogFile $fullLogFile 1>&2
+
+if [ "$?" == "0" ];then
+    colorMsg $green "[OK]"
+else
+    colorMsg $red "[DEFEATED]"
+    printf "\n"
+    printf "Build fit2openshift webconsole api  defeated! An error log in :"${errorLogFile}
+    printf "\n"
+    exit 1
+fi
+
+colorMsg $blue "Build FIT2OPENSHIFT IMAGES completed!"
