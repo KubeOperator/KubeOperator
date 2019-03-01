f9cfe62dafaf9b48eca6e94c454e6d6c71554a38
chenyang <chenyang@fit2cloud.com>
2019-03-01 13:00:41 +0800

    fix installer

--- openshift-offline-resources/okd-v3.11.0-0cbc58b/okd-3.11-meta/download-dependencies.sh
+++ openshift-offline-resources/okd-v3.11.0-0cbc58b/okd-3.11-meta/download-dependencies.sh
@@ -28,7 +28,6 @@ download ${chef} dependencies/chef-14.9.13-1.el7.x86_64.rpm
 download ${openJava} dependencies/server-jre-8u192-linux-x64.tar.gz 
 download ${nexus} nexus-okd-3.11.tar.gz
 tar -zvxf nexus-okd-3.11.tar.gz 
-if [ "$?" == "0" ];then
-else
+if [ "$?" != "0" ];then
     exit 1
 fi
