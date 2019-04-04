af94aec384618a3854a44146020196bd9d7197cd
ibuler <ibuler@qq.com>
2019-04-04 16:49:34 +0800

    [Update] 修改脚本

---
+++ scripts/3_download_resource.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+BASE_DIR=$(dirname $(dirname "$0"))
+
+
+function download_nexus_resource {
+    NEXUS_TAR_PATH="${BASE_DIR}/docker/nexus/nexus-data.tar.gz"
+    NEXUS_DATA_PATH="${BASE_DIR}/docker/nexus/data/"
+
+    if [[ ! -f "${NEXUS_TAR_PATH}" ]];then
+        wget "http://fit2openshift.oss-cn-beijing.aliyuncs.com/okd-3.11/tmp/nexus-data.tar.gz" -O ${NEXUS_DATA_PATH}
+    fi
+
+    if [[ ! -f "${NEXUS_DATA_PATH}/.done" ]];then
+        tar xvf ${NEXUS_TAR_PATH} -C ${NEXUS_DATA_PATH} && echo > ${NEXUS_DATA_PATH}/.done
+    fi
+}
+
+function main {
+    download_nexus_resource
+}
+
+main
