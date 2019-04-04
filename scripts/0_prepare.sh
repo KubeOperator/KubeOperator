402d79e812004dc5db75ebceaf579b4d2b0a894c
ibuler <ibuler@qq.com>
2019-04-04 19:36:02 +0800

    [Update] 修改prepaire

--- scripts/0_prepare.sh
+++ scripts/0_prepare.sh
@@ -53,7 +53,7 @@ function download_resources() {
     NEXUS_DATA_PATH="${BASE_DIR}/docker/nexus/data/"
 
     if [[ ! -f "${NEXUS_TAR_PATH}" ]];then
-        wget "http://fit2openshift.oss-cn-beijing.aliyuncs.com/okd-3.11/tmp/nexus-data.tar.gz" -O ${NEXUS_TAR_PATH}
+        wget "http://fit2openshift.oss-cn-beijing.aliyuncs.com/okd-3.11//tmp/nexus-data.tar.gz" -O ${NEXUS_TAR_PATH}
     fi
 }
 
