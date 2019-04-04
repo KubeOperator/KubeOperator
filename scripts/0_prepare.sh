e9a16efd5c3ad42d8d93ec01fecf138383ded6c2
ibuler <ibuler@qq.com>
2019-04-04 20:01:13 +0800

    [Update] 修改下载逻辑

--- scripts/0_prepare.sh
+++ scripts/0_prepare.sh
@@ -52,7 +52,9 @@ function download_resources() {
     NEXUS_TAR_PATH="${BASE_DIR}/docker/nexus/nexus-data.tar.gz"
     NEXUS_DATA_PATH="${BASE_DIR}/docker/nexus/data/"
 
-    if [[ ! -f "${NEXUS_TAR_PATH}" || $(du -sh nexus-data.tar.gz | grep 'G' | awk -F. '{ print $1 }') -gt 5 ]];then
+    if [[ ! -f "${NEXUS_TAR_PATH}" ]];then
+        wget "http://fit2openshift.oss-cn-beijing.aliyuncs.com/okd-3.11//tmp/nexus-data.tar.gz" -O ${NEXUS_TAR_PATH}
+    elif [[ $(du -sh nexus-data.tar.gz | grep 'G' | awk -F. '{ print $1 }') -gt 5 ]];then
         wget "http://fit2openshift.oss-cn-beijing.aliyuncs.com/okd-3.11//tmp/nexus-data.tar.gz" -O ${NEXUS_TAR_PATH}
     fi
 }
