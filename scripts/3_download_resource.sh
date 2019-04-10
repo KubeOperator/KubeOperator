cb9c7effaaf48eec75f0ae6459b996c760b7df58
ibuler <ibuler@qq.com>
2019-04-10 17:01:33 +0800

    [Update] 修改离线资源下载

--- scripts/3_download_resource.sh
+++ scripts/3_download_resource.sh
@@ -7,7 +7,7 @@ function download_nexus_resource {
     NEXUS_DATA_PATH="${PROJECT_DIR}/docker/nexus/data/"
 
     if [[ ! -f "${NEXUS_TAR_PATH}" ]];then
-        wget "http://fit2openshift.oss-cn-beijing.aliyuncs.com/okd/v3/nexus-data.tar.gz" -O ${NEXUS_TAR_PATH}
+        wget "http://fit2anything.oss-cn-beijing.aliyuncs.com/nexus/v3-11/nexus-data.tar.gz" -O ${NEXUS_TAR_PATH}
     fi
 
     if [[ ! -f "${NEXUS_DATA_PATH}/.done" ]];then
