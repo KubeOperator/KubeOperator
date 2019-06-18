afb85411455feaadc6f48a1a706ecc9874a6cc2c
shenchenyang <scydeai@qq.com>
2019-06-18 11:11:12 +0800

    change oss download

--- api/instll_curl.sh
+++ api/instll_curl.sh
@@ -1,4 +1,4 @@
-wget https://curl.haxx.se/download/curl-7.55.1.tar.gz -o /tmp/curl-7.55.1.tar.gz
+wget https://kubeoperator-public.oss-cn-beijing.aliyuncs.com/dependencies/curl-7.55.1.tar.gz -o /tmp/curl-7.55.1.tar.gz
 cd /tmp
 tar -xzvf  curl-7.55.1.tar.gz
 cd curl-7.55.1
