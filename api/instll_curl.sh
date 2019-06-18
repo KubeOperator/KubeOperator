b714da937519fff7a62108624daf4f46e1c10da1
shenchenyang <scydeai@qq.com>
2019-06-18 10:53:54 +0800

    update install script

--- api/instll_curl.sh
+++ api/instll_curl.sh
@@ -1,4 +1,4 @@
-wget https://curl.haxx.se/download/curl-7.55.1.tar.gz -o /tmp
+wget https://curl.haxx.se/download/curl-7.55.1.tar.gz -o /tmp/curl-7.55.1.tar.gz
 cd /tmp
 tar -xzvf  curl-7.55.1.tar.gz
 cd curl-7.55.1
