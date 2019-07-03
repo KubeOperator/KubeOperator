d9a5f03b3e8c1e767a951bcc90021f4298a25773
shenchenyang <scydeai@qq.com>
2019-07-03 14:58:38 +0800

    change rpm

--- api/resource/requirements/build.sh
+++ api/resource/requirements/build.sh
@@ -1,8 +1,5 @@
-images_list=$1
-rmp_list=$2
-
 printf "开始处理镜像..."
-for i in $(cat image_list);
+for i in $(cat $1);
 do
   docker pull $i;
   docker tag localhost:8082/$i localhost:8082/$i;
@@ -11,9 +8,9 @@ done
 printf "镜像处理完毕!"
 
 printf "开始处理rpm"
-for r in $(cat rmp_list);
+for r in $(cat $2);
 do
-  yum install r -y
+  yumdownloader r
 done
 printf "rpm处理完毕！"
 
