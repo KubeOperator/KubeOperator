4dcd65dda5c96ce0003fd5e55916bb41a8954262
shenchenyang <scydeai@qq.com>
2019-07-14 22:20:31 +0800

    修改安装脚本

--- scripts/4_install.sh
+++ scripts/4_install.sh
@@ -5,8 +5,7 @@ source ${BASE_DIR}/utils.sh
 function main() {
     ${SCRIPTS_DIR}/1_set_iptables.sh
     ${SCRIPTS_DIR}/2_install_docker.sh
-    ${SCRIPTS_DIR}/3_download_resource.sh
-    ${SCRIPTS_DIR}/4_prepare_images.sh
+    ${SCRIPTS_DIR}/3_prepare_images.sh
 }
 
 main
