12e73044688a5dd5e58b86443ca112147c859ce9
shenchenyang <scydeai@qq.com>
2019-07-19 17:49:00 +0800

    展示默认信息

--- scripts/4_install.sh
+++ scripts/4_install.sh
@@ -2,10 +2,17 @@
 BASE_DIR=$(dirname "$0")
 source ${BASE_DIR}/utils.sh
 
+function success(){
+    echo -e "\033[31m kubeOperator 安装成功!\n 默认登陆信息: \033[0m"  
+    echo -e "\033[32m username: admin \033[0m"  
+    echo -e "\033[32m password: kubeoperator@admin123 \033[0m"  
+}
+
 function main() {
     ${SCRIPTS_DIR}/1_set_iptables.sh
     ${SCRIPTS_DIR}/2_install_docker.sh
-    ${SCRIPTS_DIR}/3_prepare_images.sh
+    ${SCRIPTS_DIR}/3_prepare_images.sh  
+    success
 }
 
 main
