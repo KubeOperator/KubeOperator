6ea444a676f818613f99a8e46404a38eafefec03
shenchenyang <scydeai@qq.com>
2019-08-23 16:20:45 +0800

    fix bug

--- api/kubeops_api/components.py
+++ api/kubeops_api/components.py
@@ -1,5 +1,3 @@
-import requests
-
 http_prefix = 'http://'
 https_prefix = 'https://'
 
@@ -8,88 +6,12 @@ def get_component_urls(cluster):
     urls = {}
     app_url = cluster.get_config("APP_DOMAIN").get('value')
     if app_url:
-        grafana_urls = generate_grafana_urls(cluster, app_url)
-        urls.update(grafana_urls)
-        prometheus_urls = generate_prometheus_url(cluster, app_url)
-        urls.update(prometheus_urls)
-        registry_urls = generate_registry_url(cluster, app_url)
-        urls.update(registry_urls)
-        dashboard_urls = generate_dashboard_url(cluster, app_url)
-        urls.update(dashboard_urls)
-        tarefis_urls = generate_tarefik_url(cluster, app_url)
-        urls.update(tarefis_urls)
-        scope_urls = generate_scope_url(cluster, app_url)
-        urls.update(scope_urls)
+        urls = {
+            "grafana": http_prefix + "grafana." + app_url,
+            "prometheus": http_prefix + "prometheus." + app_url,
+            "registry-ui": http_prefix + "registry-ui." + app_url,
+            "dashboard": https_prefix + "dashboard." + app_url,
+            "traefik": http_prefix + "traefik." + app_url,
+            "scope": https_prefix + "scope.weave." + app_url
+        }
     return urls
-
-
-def generate_grafana_urls(cluster, app_url):
-    db_url = http_prefix + 'grafana.' + app_url
-    urls = {"grafana": db_url}
-    if cluster.status == 'RUNNING':
-        urls.update(list_grafana_dbs(db_url))
-    return urls
-
-
-def list_grafana_dbs(db_url):
-    urls = {}
-    etcd_title = 'Etcd by Prometheus'
-    apps_title = 'Kubernetes Apps'
-    cluster_health_title = 'Kubernetes Cluster Health'
-    nodes_title = 'Kubernetes Nodes (prometheus)'
-    cluster_title = 'Cluster Monitoring for Kubernetes'
-    try:
-        res = requests.get(db_url + "/api/search", timeout=5)
-        dbs = res.json()
-        for db in dbs:
-            url = db_url + db.get('url')
-            if db.get('title') == etcd_title:
-                urls['etcd_grafana'] = url
-            elif db.get('title') == apps_title:
-                urls['apps_grafana'] = url
-            elif db.get('title') == cluster_health_title:
-                urls['cluster_health_grafana'] = url
-            elif db.get('title') == nodes_title:
-                urls['nodes_grafana'] = url
-            elif db.get('title') == cluster_title:
-                urls['cluster_grafana'] = url
-    except Exception as e:
-        urls = {}
-    return urls
-
-
-def generate_prometheus_url(cluster, app_url):
-    prometheus_url = http_prefix + "prometheus." + app_url
-    return {
-        "prometheus": prometheus_url
-    }
-
-
-def generate_registry_url(cluster, app_url):
-    registry_url = http_prefix + "registry." + app_url
-    registry_ui_url = http_prefix + "registry-ui." + app_url
-    return {
-        "registry": registry_url,
-        "registry-ui": registry_ui_url
-    }
-
-
-def generate_dashboard_url(cluster, app_url):
-    dashboard_url = https_prefix + "dashboard." + app_url
-    return {
-        "dashboard": dashboard_url
-    }
-
-
-def generate_tarefik_url(cluster, app_url):
-    traefik_url = http_prefix + "traefik." + app_url
-    return {
-        "traefik": traefik_url
-    }
-
-
-def generate_scope_url(cluster, app_url):
-    scope_url = http_prefix + "scope." + app_url
-    return {
-        "scope": scope_url
-    }
