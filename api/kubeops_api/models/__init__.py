2d9edb336a1c8932759f0b6a079fabb32dd7581f
wangzhengkun <zhengkun@fit2cloud.com>
2019-09-18 18:34:41 +0800

    fix bug

--- api/kubeops_api/models/__init__.py
+++ api/kubeops_api/models/__init__.py
@@ -1,2 +1,3 @@
 from .credential import *
+from .backup_storage import *
 from . import *
--- api/kubeops_api/models/backup_storage.py
+++ api/kubeops_api/models/backup_storage.py
@@ -2,14 +2,15 @@ import logging
 import uuid
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# from common.models import JsonTextField
+from common.models import JsonDictTextField
 
-__all__ = ["BackupStorage"]
+__all__ = ["backup_storage"]
 
 
-class BackupStorage(models.Model):
-    BACKUP_STORAGE_STATUS_VALID = 'VALID'
+class backup_storage(models.Model):
+    BACKUP_STORAGE_STATUS_VALID = 'VALID1233'
     BACKUP_STORAGE_STATUS_INVALID = 'INVALID'
 
     BACKUP_STORAGE_STATUS_CHOICES  = (
@@ -27,8 +28,8 @@ class BackupStorage(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     name = models.CharField(max_length=128, null=True,blank=True)
-    region = models.CharField(max_length=128,null=True,blank=True)
-    # credentials = JsonDictTextField(blank=True, null=True)
+    region1 = models.CharField(max_length=128,null=True,blank=True)
+    credentials = JsonDictTextField(blank=True, null=True)
     type = models.CharField(max_length=64,choices=BACKUP_STORAGE_TYPE_CHOICES)
     status = models.CharField(max_length=64,choices=BACKUP_STORAGE_STATUS_CHOICES,default=BACKUP_STORAGE_STATUS_VALID)
     date_created = models.DateTimeField(auto_now_add=True, verbose_name=_('Date created'))
