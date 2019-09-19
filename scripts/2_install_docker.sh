6a5c54d1d687d56960cfe86fd593999f504818d0
shenchenyang <scydeai@qq.com>
2019-09-19 12:09:50 +0800

    install docker-compose

--- scripts/2_install_docker.sh
+++ scripts/2_install_docker.sh
@@ -5,19 +5,32 @@ OS=$(uname)
 
 OFFLINE_DOCKER_DIR="${PROJECT_DIR}/docker/bin"
 
-function install_docker_online {
-    yum -y remove docker docker-common docker-engine
+function prepare_install {
     yum install -y epel-release yum-utils device-mapper-persistent-data lvm2
+}
+
+
+function install_docker_online {
+    prepare_install
+    yum -y remove docker docker-common docker-engine
     wget -O /etc/yum.repos.d/docker-ce.repo https://download.docker.com/linux/centos/docker-ce.repo
     sed -i 's+download.docker.com+mirrors.tuna.tsinghua.edu.cn/docker-ce+' /etc/yum.repos.d/docker-ce.repo
-    yum install -y docker-ce docker-compose
+    yum install -y docker-ce 
+}
+
+function install_docker-compose_online {
+    yum install -y docker-compose
 }
 
 function install_docker_offline {
     cp ${OFFLINE_DOCKER_DIR}/docker/docker* /usr/bin/
-    cp ${OFFLINE_DOCKER_DIR}/docker-compose /usr/bin/
     cp ${OFFLINE_DOCKER_DIR}/docker.service /etc/systemd/system/
     chmod +x /usr/bin/docker* && chmod 754 /etc/systemd/system/docker.service
+}
+
+function install_docker-compose_offline {
+    cp ${OFFLINE_DOCKER_DIR}/docker-compose /usr/bin/
+    chmod +x /usr/bin/docker* 
 }
 
 function install_docker {
@@ -30,6 +43,19 @@ function install_docker {
         install_docker_offline
     else
         install_docker_online
+    fi
+}
+
+function install_docker-compose {
+    echo ">> Install docker-compose"
+    if [[ "${OS}" == "Darwin" ]];then
+        echo "Platform is MacOS, install manually"
+        return
+    fi
+    if [[ -f "${OFFLINE_DOCKER_DIR}/docker/docker-compose" ]];then
+        install_docker-compose_offline
+    else
+        install_docker-conpose_online
     fi
 }
 
@@ -48,6 +74,10 @@ function main {
        install_docker
        config_docker
        start_docker
+    fi
+    which docker-compose >/dev/null 2>&1
+    if [ $? -ne 0];then
+       install_docker-compose
     fi
 }
 
