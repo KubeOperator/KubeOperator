f4482ff1df01ae8b3f6c23f1d53f453a2aec56e7
wangzhengkun <zhengkun@fit2cloud.com>
2019-09-23 16:57:04 +0800

    增加OSS和Azure支持

--- api/kubeops_api/storage_client.py
+++ api/kubeops_api/storage_client.py
@@ -6,15 +6,17 @@ class StorageClient():
 
     def check_valid(self,backupStorage):
         storage_config = self.coverToConfig(self,backupStorage['credentials'])
+        client = {}
         if 'S3' == backupStorage['type']:
-            return self.check_s3(self,storage_config)
-
-
-    def check_s3(self,storage_config):
-        client= jms_storage.S3Storage(storage_config)
-        # 上传文件测试可用性
-        file_locale_path = os.path.abspath(os.path.join(os.getcwd(), os.path.pardir, 'README.md'))
-        return client.is_valid(file_locale_path,'kube-operator-test')
+            client = jms_storage.S3Storage(storage_config)
+        if 'OSS' == backupStorage['type']:
+            client = jms_storage.OSSStorage(storage_config)
+        if 'AZURE' == backupStorage['type']:
+            client = jms_storage.AzureStorage(storage_config)
+        if client is not None:
+            # 上传文件测试可用性
+            file_locale_path = os.path.abspath(os.path.join(os.getcwd(), os.path.pardir, 'README.md'))
+            return client.is_valid(file_locale_path, 'kube-operator-test')
 
 
     def coverToConfig(self,credentials):
@@ -26,4 +28,5 @@ class StorageClient():
         storage_config['ACCOUNT_NAME'] = credentials.get('accountName',None)
         storage_config['ACCOUNT_KEY'] = credentials.get('accountSecret',None)
         storage_config['ENDPOINT_SUFFIX'] = credentials.get('endpointSuffix',None)
+        storage_config['ENDPOINT'] = credentials.get('endpoint',None)
         return storage_config
