bb42b3fa5aba60f114fbe2553f51a326a24924e9
shenchenyang <scydeai@qq.com>
2019-09-25 16:31:47 +0800

    现实hostregion

--- api/kubeops_api/models/host.py
+++ api/kubeops_api/models/host.py
@@ -38,7 +38,7 @@ class Host(BaseHost):
     @property
     def region(self):
         if self.zone:
-            return self.zone.region
+            return self.zone.region.name
 
     def gather_info(self):
         facts = gather_host_info(self.ip, self.username, self.password)
