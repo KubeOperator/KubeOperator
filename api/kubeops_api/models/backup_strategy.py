21b4db170b10e29a525f4f4b118353ac34250cf3
wangzhengkun <zhengkun@fit2cloud.com>
2019-10-10 11:11:06 +0800

    数据库字段修改

--- api/kubeops_api/models/backup_strategy.py
+++ api/kubeops_api/models/backup_strategy.py
@@ -22,5 +22,5 @@ class BackupStrategy(models.Model):
     # backup_storage = models.ForeignKey('BackupStorage',on_delete=models.SET_NULL,null=True)
     # project = models.ForeignKey('ansible_api.project',on_delete=models.CASCADE)
     project_id = models.CharField(max_length=64,null=True,blank=True,unique=True)
-    status = models.CharField(max_length=64,null=False,blank=False,choices=BACKUP_STORAGE_STATUS_CHOICES)
+    status = models.CharField(max_length=64,null=False,blank=False,choices=BACKUP_STORAGE_STATUS_CHOICES,default=BACKUP_STRATEGY_STATUS_DISABLE)
 
