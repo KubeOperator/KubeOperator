12841998976708dbc895fb8347a5f9ec7b120ac0
wangzhengkun <zhengkun@fit2cloud.com>
2019-10-10 11:12:36 +0800

    数据库字段修改

--- api/kubeops_api/migrations/0028_auto_20191010_0310.py
+++ api/kubeops_api/migrations/0028_auto_20191010_0310.py
@@ -11,10 +11,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='package',
-            name='endpoint',
-        ),
         migrations.AddField(
             model_name='backupstrategy',
             name='status',
