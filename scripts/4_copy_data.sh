ad44e23621783b5e85550187d1c1a017f9c2a88f
shenchenyang <scydeai@qq.com>
2019-10-12 16:12:40 +0800

    update scripts

--- scripts/4_copy_data.sh
+++ scripts/4_copy_data.sh
@@ -5,11 +5,11 @@ source ${BASE_DIR}/utils.sh
 
 
 function main {
-    mkdir -p /opt/kubeOperator >/dev/null 2>&1
+    mkdir -p /opt/kubeoperator >/dev/null 2>&1
     cp -rp ${SCRIPTS_DIR}/service/kubeops.service /etc/systemd/system/
-    cp -rp ${PROJECT_DIR}/docker /opt/kubeOperator/
-    cp -rp ${PROJECT_DIR}/scripts /opt/kubeOperator/
-    cp -rp ${PROJECT_DIR}/docker-compose.yml /opt/kubeOperator/
-    cp -rp ${PROJECT_DIR}/kubeopsctl.sh /opt/kubeOperator/
+    cp -rp ${PROJECT_DIR}/scripts /opt/kubeoperator/
+    cp -rp ${PROJECT_DIR}/docker-compose.yml /opt/kubeoperator/
+    cp -rp ${PROJECT_DIR}/kubeopsctl.sh /opt/kubeoperator/
+    cp -rp ${PROJECT_DIR}/data /opt/kubeoperator/
 }
 main
