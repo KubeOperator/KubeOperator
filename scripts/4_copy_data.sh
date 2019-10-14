a5e629d282adca9b2929a2c69275fb72156f88c7
shenchenyang <scydeai@qq.com>
2019-10-14 12:19:43 +0800

    fix bug

--- scripts/4_copy_data.sh
+++ scripts/4_copy_data.sh
@@ -7,6 +7,6 @@ source ${BASE_DIR}/utils.sh
 function main {
     mkdir -p /opt/kubeoperator >/dev/null 2>&1
     cp -rp ${SCRIPTS_DIR}/service/kubeops.service /etc/systemd/system/
-    cp -rp ${PROJECT_DIR}/scripts/* /opt/kubeoperator/
+    cp -rp ${PROJECT_DIR}/* /opt/kubeoperator/
 }
 main
