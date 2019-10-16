8167e377b29178fa9d05f8b4c57413efd6526ad4
wangzhengkun <zhengkun@fit2cloud.com>
2019-10-16 14:59:09 +0800

    增加集群健康统计定时任务

---
+++ api/kubeops_api/cluster_health_utils.py
@@ -0,0 +1,37 @@
+from kubeops_api.models.cluster import Cluster
+from kubeops_api.models.setting import Setting
+from kubeops_api.prometheus_client import PrometheusClient
+from kubeops_api.models.cluster_health_history import ClusterHealthHistory
+
+import datetime
+
+
+def get_cluster_health_msg_hour():
+    clusters = Cluster.objects.all()
+    for cluster in clusters:
+        domain_suffix = Setting.objects.get(key="domain_suffix")
+        host = "prometheus.apps."+cluster.name+"."+domain_suffix.value
+        config = {
+            'host': host
+        }
+        prometheus_client = PrometheusClient(config)
+        result = prometheus_client.handle_targets_message(prometheus_client.targets())
+        if result['success']:
+            month = datetime.datetime.now().strftime('%Y-%m')
+            clusterHealthHistory = ClusterHealthHistory(project_id=cluster.id,available_rate=result['rate'],
+                                                        date_type=ClusterHealthHistory.CLUSTER_HEALTH_HISTORY_DATE_TYPE_HOUR,
+                                                        month=month)
+            clusterHealthHistory.save()
+
+def handle_cluster_health_msg_day():
+    hour_msg = ClusterHealthHistory.objects.filter(date_type=ClusterHealthHistory.CLUSTER_HEALTH_HISTORY_DATE_TYPE_HOUR)
+    cluster_rate = 0
+    for hour in hour_msg:
+        cluster_rate = cluster_rate+hour.available_rate
+    cluster_rate = cluster_rate / len(hour_msg)
+    month = datetime.datetime.now().strftime('%Y-%m')
+    clusterHealthHistory = ClusterHealthHistory(project_id=hour_msg[0].project_id,available_rate=cluster_rate,
+                                                date_type=ClusterHealthHistory.CLUSTER_HEALTH_HISTORY_DATE_TYPE_DAY,
+                                                month=month)
+    clusterHealthHistory.save()
+    ClusterHealthHistory.objects.filter(ClusterHealthHistory.CLUSTER_HEALTH_HISTORY_DATE_TYPE_HOUR).delete()
--- api/kubeops_api/tasks.py
+++ api/kubeops_api/tasks.py
@@ -8,6 +8,7 @@ from ansible_api.ctx import change_to_root
 from kubeops_api.models.cluster import Cluster
 from kubeops_api.models.deploy import DeployExecution
 import kubeops_api.cluster_backup_utils
+import kubeops_api.cluster_health_utils
 from celery.schedules import crontab
 
 
@@ -40,3 +41,11 @@ def test_task():
 @periodic_task(run_every=crontab(minute='*/15'),name='task.cluster_backup')
 def cluster_backup():
     kubeops_api.cluster_backup_utils.cluster_backup()
+
+@periodic_task(run_every=crontab(minute=0, hour=0),name='task.get_cluster_health_day')
+def get_cluster_health_day():
+    kubeops_api.cluster_health_utils.handle_cluster_health_msg_day()
+
+@periodic_task(run_every=crontab(hour='*/1'),name='task.get_cluster_health_hour')
+def get_cluster_health_hour():
+    kubeops_api.cluster_health_utils.get_cluster_health_msg_hour()
