6cd8aa2180a3a86f90c60790b705880398535269
shenchenyang <scydeai@qq.com>
2019-10-17 10:23:11 +0800

    chmod data

--- scripts/4_copy_data.sh
+++ scripts/4_copy_data.sh
@@ -8,5 +8,6 @@ function main {
     mkdir -p /opt/kubeoperator >/dev/null 2>&1
     cp -rp ${SCRIPTS_DIR}/service/kubeops.service /etc/systemd/system/
     cp -rp ${PROJECT_DIR}/* /opt/kubeoperator/
+    chmod -R 777 /opt/kubeoperator/data
 }
 main
