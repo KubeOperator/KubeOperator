b07feff6f782d89bac8544dc7000a9c448cbc656
shenchenyang <scydeai@qq.com>
2019-10-24 16:40:50 +0800

    add log detail

--- ui/src/app/log/log-detail/log-detail.component.ts
+++ ui/src/app/log/log-detail/log-detail.component.ts
@@ -12,18 +12,29 @@ export class LogDetailComponent implements OnInit {
   @Input() showLogModal = false;
   @Output() showLogModalChange = new EventEmitter();
   @Input() logUrl = null;
-  logText = '';
+  @ViewChild('terminal', {static: true}) terminal: ElementRef;
+  term: Terminal;
 
   constructor(private service: LogService) {
   }
 
   ngOnInit() {
+    this.term = new Terminal({
+      cursorBlink: false,
+      disableStdin: true,
+      cursorStyle: 'bar',
+      cols: 120,
+      rows: 30,
+      letterSpacing: 1,
+      fontSize: 16
+    });
+    this.term.open(this.terminal.nativeElement);
   }
 
   loadLog() {
     if (this.logUrl != null) {
       this.service.getExecutionLog(this.logUrl).subscribe(data => {
-        this.logText = data.data;
+        this.term.write(data.data);
       });
     }
   }
