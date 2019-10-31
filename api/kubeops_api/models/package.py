63b5a787b60d7d6b30124a5aa3de164fd3c4966e
shenchenyang <scydeai@qq.com>
2019-10-31 12:45:26 +0800

    Revert "update ui"
    This reverts commit 0891b78f
    
    (cherry picked from commit 4b30f1f38bca69be70670f45af4edc1673c3559e)

--- api/kubeops_api/models/package.py
+++ api/kubeops_api/models/package.py
@@ -6,7 +6,7 @@ import yaml
 from django.db import models
 from common.models import JsonTextField
 from django.utils.translation import ugettext_lazy as _
-from fit2ansible.settings import PACKAGE_DIR
+from fit2ansible.settings import PACKAGE_DIR, DEV, DEV_PACKAGE_DIR
 from kubeops_api.package_manage import *
 
 __all__ = ['Package']
@@ -17,7 +17,6 @@ class Package(models.Model):
     name = models.CharField(max_length=20, unique=True, verbose_name=_('Name'))
     meta = JsonTextField(blank=True, null=True, verbose_name=_('Meta'))
     date_created = models.DateTimeField(auto_now_add=True, verbose_name=_('Date created'))
-    packages_dir = PACKAGE_DIR
 
     def __str__(self):
         return self.name
@@ -25,11 +24,6 @@ class Package(models.Model):
     class Meta:
         verbose_name = _('Package')
 
-    @property
-    def path(self):
-        return os.path.join(self.packages_dir, self.name)
-
-    @property
     def repo_port(self):
         return self.meta['vars']['repo_port']
 
@@ -39,8 +33,11 @@ class Package(models.Model):
 
     @classmethod
     def lookup(cls):
-        for d in os.listdir(cls.packages_dir):
-            full_path = os.path.join(cls.packages_dir, d)
+        packages_dir = PACKAGE_DIR
+        if DEV:
+            packages_dir = DEV_PACKAGE_DIR
+        for d in os.listdir(packages_dir):
+            full_path = os.path.join(packages_dir, d)
             meta_path = os.path.join(full_path, 'meta.yml')
             if not os.path.isdir(full_path) or not os.path.isfile(meta_path):
                 continue
@@ -48,8 +45,9 @@ class Package(models.Model):
                 metadata = yaml.load(f)
             defaults = {'name': d, 'meta': metadata}
             instance = cls.objects.update_or_create(defaults=defaults, name=d)[0]
-            thread = threading.Thread(target=cls.start_container(instance))
-            thread.start()
+            if not DEV:
+                thread = threading.Thread(target=cls.start_container(instance))
+                thread.start()
 
     @classmethod
     def start_container(cls, package):
