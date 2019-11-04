721146e4293e86bad38457f1840107a21b17c187
xuwei <wei@fit2cloud.com>
2019-11-04 17:15:55 +0800

    fix uninstall script

--- scripts/6_uninstall.sh
+++ scripts/6_uninstall.sh
@@ -6,16 +6,24 @@ function success(){
     echo -e "\033[32m kubeOperator 卸载完成. \033[0m"
 }
 function remove_dir() {
-    rm -rf /opt/kubeOperator 2&> /dev/null
+    echo -e "删除 KubeOperator 工作目录"
+    rm -rf /opt/kubeoperator 2&> /dev/null
+}
+function remove_service() {
+    echo -e "停止 KubeOperator 服务进程"
+    systemctl stop kubeops
+    systemctl disable kubeops
+    rm -rf /etc/systemd/system/kubeops.service
 }
 
 function remove_images() {
-    echo -e "停止KubeOperator服务进程"
-    cd ${PROJECT_DIR} && ${PROJECT_DIR}/kubeopsctl.sh down
+    echo -e "删除 docker 镜像"
+    docker ps -a | grep 'nexus-helm' | awk  '{print $1}'|xargs docker rm -f 2&> /dev/null
     docker images|grep -v IMAGE|awk '{print $3}'|xargs docker rmi 2&> /dev/null
 }
 
 function main() {
+    remove_service
     remove_images
     remove_dir
     success
