dd8e45b3c6803a037518f3038e81eddd809b852a
shenchenyang <scydeai@qq.com>
2019-11-05 17:32:03 +0800

    disable kubeoperator iptables

--- scripts/1_set_iptables.sh
+++ scripts/1_set_iptables.sh
@@ -2,27 +2,16 @@
 success=0
 
 function set_firewall {
-    firewall-cmd --zone=public --add-port=80/tcp --add-port=8080/tcp  --add-port=8080-8089/tcp --add-port=8090-8099/tcp --permanent >/dev/null 2>&1
-    systemctl restart firewalld >/dev/null 2>&1
+    systemctl stop firewalld >/dev/null 2>&1
+    systemctl disable  firewalld >/dev/null 2>&1
+
 }
 
-function set_iptables {
-    iptables -I INPUT -p tcp -port 80 -j ACCEPT
-    iptables -I INPUT -p tcp -port 8080 -j ACCEPT
-    iptables -I INPUT -p tcp -port 8082 -j ACCEPT
-    iptables -I INPUT -p tcp -port 8092 -j ACCEPT
-}
 
 function main {
     which firewall-cmd &> /dev/null
     if [[ "$?" == "0" ]];then
         set_firewall
-        exit 0
-    fi
-
-    which iptables &> /dev/null
-    if [[ "$?" == "0" ]];then
-        set_iptables
         exit 0
     fi
 }
