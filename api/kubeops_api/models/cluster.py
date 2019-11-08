7527dfb886cc38a20bf50f55499852edd8ca41cf
shenchenyang <scydeai@qq.com>
2019-11-08 17:56:44 +0800

    fix bug
    (cherry picked from commit 5c56d5b5b31ff3ba3d28b9162dfdc22e96b8a95c)

--- api/kubeops_api/models/cluster.py
+++ api/kubeops_api/models/cluster.py
@@ -193,7 +193,7 @@ class Cluster(Project):
                     vars = storage.get('vars', {})
             if self.persistent_storage == 'nfs':
                 nfs = NfsStorage.objects.get(name=self.configs['nfs'])
-                nfs.vars.pop('repo_port')
+                nfs.vars.pop('repo_port', None)
                 vars.update(nfs.vars)
             self.set_config_unlock(vars)
 
