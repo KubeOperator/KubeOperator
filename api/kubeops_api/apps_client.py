91d49b8804ccf51664aa8fa18570f73706b8ed34
shenchenyang <scydeai@qq.com>
2019-11-20 18:36:38 +0800

    appclient

---
+++ api/kubeops_api/apps_client.py
@@ -0,0 +1,15 @@
+import requests
+
+
+class AppsClient():
+    def __init__(self, cluster):
+        self.cluster = cluster
+
+    def get(self, app, url):
+        app_domain = app + "." + self.cluster.get_config('APP_DOMAIN')
+        header = {
+            "Host": app_domain
+        }
+        host_ip = self.cluster.get_first_master()
+        req_url = str(url).replace(app_domain, host_ip)
+        return requests.get(header=header, url=req_url)
