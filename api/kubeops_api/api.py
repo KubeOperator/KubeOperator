e850c4a7103b75dc39341ec50486a0808c87573a
wangzhengkun <zhengkun@fit2cloud.com>
2019-11-22 15:22:19 +0800

    fix bug

--- api/kubeops_api/api.py
+++ api/kubeops_api/api.py
@@ -488,7 +488,7 @@ class ClusterStorageView(APIView):
     def get(self, request, *args, **kwargs):
         project_name = kwargs['project_name']
         cluster = Cluster.objects.get(name=project_name)
-        if cluster.status == Cluster.CLUSTER_STATUS_READY:
+        if cluster.status == Cluster.CLUSTER_STATUS_READY and cluster.status == Cluster.CLUSTER_STATUS_INSTALLING:
             return Response(data={'msg': ': 集群未创建'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         cluster_monitor = ClusterMonitor(cluster)
         result = cluster_monitor.list_storage_class()
