5de8acaff156ed33fbb2ad0a7bf7fc561d26d2b9
wangzhengkun <zhengkun@fit2cloud.com>
2019-12-06 13:34:08 +0800

    增加ceph级联删除

--- api/storage/api.py
+++ api/storage/api.py
@@ -1,8 +1,9 @@
-from rest_framework import viewsets
-
 from ansible_api.permissions import IsSuperUser
 from storage import serializers
-from storage.models import NfsStorage,CephStorage
+from storage.models import NfsStorage, CephStorage, ClusterCephStorage
+from rest_framework.response import Response
+from rest_framework import viewsets, status
+
 
 
 class NfsStorageViewSet(viewsets.ModelViewSet):
@@ -12,9 +13,18 @@ class NfsStorageViewSet(viewsets.ModelViewSet):
     lookup_field = 'name'
     lookup_url_kwarg = 'name'
 
+
 class CephStorageViewSet(viewsets.ModelViewSet):
     queryset = CephStorage.objects.all()
     serializer_class = serializers.CephStorageSerializer
     permission_classes = (IsSuperUser,)
     lookup_field = 'name'
     lookup_url_kwarg = 'name'
+
+    def destroy(self, request, *args, **kwargs):
+        ceph_storage = self.get_object()
+        cluster_ceph_storage = ClusterCephStorage.objects.filter(ceph_storage_id=ceph_storage.id)
+        if len(cluster_ceph_storage) > 0:
+            return Response(data={'msg': '有集群使用此ceph 不可删除!'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return super().destroy(self, request, *args, **kwargs)
