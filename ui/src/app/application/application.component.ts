1fd5a6e3e034939ada02e94b733550c97f72eb8a
wangzhengkun <zhengkun@fit2cloud.com>
2019-12-30 14:05:47 +0800

    内置应用页增加域名解析规则复制按钮

--- ui/src/app/application/application.component.ts
+++ ui/src/app/application/application.component.ts
@@ -1,10 +1,10 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Cluster, ClusterConfigs} from '../cluster/cluster';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Cluster} from '../cluster/cluster';
 import {App} from '../package/package';
-import {PackageService} from '../package/package.service';
 import {ActivatedRoute} from '@angular/router';
 import {ClusterService} from '../cluster/cluster.service';
-import {print} from 'util';
+import {NodeService} from '../node/node.service';
+import * as clipboard from 'clipboard-polyfill';
 
 @Component({
   selector: 'app-application',
@@ -15,8 +15,13 @@ export class ApplicationComponent implements OnInit {
 
   apps: App[] = [];
   currentCluster: Cluster;
+  workers = [];
+  workerIp = '';
+  @ViewChild('alertModal', {static: true}) alertModal;
+  openHost = false;
 
-  constructor(private clusterService: ClusterService, private route: ActivatedRoute) {
+
+  constructor(private clusterService: ClusterService, private route: ActivatedRoute, private nodeService: NodeService) {
   }
 
   ngOnInit() {
@@ -34,12 +39,38 @@ export class ApplicationComponent implements OnInit {
           });
         });
       });
+      this.nodeService.listNodes(this.currentCluster.name).subscribe(data => {
+        this.workers = data.filter((node) => {
+          return node.roles.includes('worker');
+        });
+        if (this.workers.length > 0) {
+          this.workerIp = this.workers[0].ip;
+        }
+      });
     });
   }
 
 
   getAppUrl(app: App) {
     return this.currentCluster.apps[app.url_key];
+  }
+
+  copy(text: string) {
+    const success = clipboard.writeText(text);
+    if (success) {
+      const alert = this.alertModal;
+      this.alertModal.showTip(false, '复制成功');
+      this.sleep(1000).then(function (this) {
+        alert.closeTip();
+      });
+    }
+  }
+
+  sleep(ms) {
+    return new Promise(
+      (resolve) => {
+        setTimeout(resolve, ms);
+      });
   }
 
 }
