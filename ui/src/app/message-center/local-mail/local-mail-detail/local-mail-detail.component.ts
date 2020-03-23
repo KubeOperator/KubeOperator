467161c8b47303ab9f7c719e8a23b6c539180847
wangzhengkun <zhengkun@fit2cloud.com>
2020-03-23 14:43:25 +0800

    增加跳转

--- ui/src/app/message-center/local-mail/local-mail-detail/local-mail-detail.component.ts
+++ ui/src/app/message-center/local-mail/local-mail-detail/local-mail-detail.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-local-mail-detail',
@@ -10,7 +11,7 @@ export class LocalMailDetailComponent implements OnInit {
   open = false;
   message: any;
 
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit() {
@@ -18,5 +19,17 @@ export class LocalMailDetailComponent implements OnInit {
 
   cancel() {
     this.open = false;
+  }
+
+  toPage(url) {
+    this.redirect(url);
+  }
+
+
+  redirect(url: string) {
+    if (url) {
+      const linkUrl = [url];
+      this.router.navigate(linkUrl);
+    }
   }
 }
