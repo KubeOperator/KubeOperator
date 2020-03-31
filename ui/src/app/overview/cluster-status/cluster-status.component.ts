54912a2bd9cd5086637dec583035644a2c5a6c08
chenyang <chenyang@fit2cloud.com>
2020-03-31 18:09:41 +0800

    add workers

--- ui/src/app/overview/cluster-status/cluster-status.component.ts
+++ ui/src/app/overview/cluster-status/cluster-status.component.ts
@@ -10,107 +10,112 @@ import {AddWorkerComponent} from '../add-worker/add-worker.component';
 import {RemoveWorkerComponent} from '../remove-worker/remove-worker.component';
 import {ClusterService} from '../../cluster/cluster.service';
 import {SessionService} from '../../shared/session.service';
+import {stringify} from '@angular/compiler/src/util';
 
 
 @Component({
-  selector: 'app-cluster-status',
-  templateUrl: './cluster-status.component.html',
-  styleUrls: ['./cluster-status.component.css']
+    selector: 'app-cluster-status',
+    templateUrl: './cluster-status.component.html',
+    styleUrls: ['./cluster-status.component.css']
 })
 export class ClusterStatusComponent implements OnInit {
 
-  @Input() currentCluster: Cluster;
-  workers: Node[] = [];
-  @ViewChild(ScaleComponent, {static: true}) scale: ScaleComponent;
-  @ViewChild(AddWorkerComponent, {static: true}) addWorker: AddWorkerComponent;
-  @ViewChild(RemoveWorkerComponent, {static: true}) removeWorker: RemoveWorkerComponent;
-  componentData = [];
-  loading = false;
-  permission;
+    @Input() currentCluster: Cluster;
+    workers: Node[] = [];
+    @ViewChild(ScaleComponent, {static: true}) scale: ScaleComponent;
+    @ViewChild(AddWorkerComponent, {static: true}) addWorker: AddWorkerComponent;
+    @ViewChild(RemoveWorkerComponent, {static: true}) removeWorker: RemoveWorkerComponent;
+    componentData = [];
+    loading = false;
+    permission;
 
-  constructor(private nodeService: NodeService, private clusterHealthService: ClusterHealthService,
-              private router: Router, private operaterService: OperaterService, private clusterService: ClusterService,
-              private sessionService: SessionService) {
-  }
-
-  ngOnInit() {
-    this.permission = this.sessionService.getItemPermission(this.currentCluster.item_name);
-    this.nodeService.listNodes(this.currentCluster.name).subscribe(data => {
-      this.workers = data.filter((node) => {
-        return node.roles.includes('worker');
-      });
-    });
-    this.getClusterStatus();
-  }
-
-  handleScale() {
-    const params = {'num': this.scale.worker_size};
-    this.operaterService.executeOperate(this.currentCluster.name, 'scale', params).subscribe(() => {
-      this.redirect('deploy');
-    }, error => {
-      this.scale.opened = false;
-    });
-  }
-
-  refresh() {
-    this.clusterService.getCluster(this.currentCluster.name).subscribe(data => {
-      this.currentCluster = data;
-    });
-  }
-
-  handleAddWorker() {
-    // const params = {'host': this.addWorker.host};
-    // this.operaterService.executeOperate(this.currentCluster.name, 'add-worker', params).subscribe(() => {
-    //   this.redirect('deploy');
-    // }, error => {
-    //   this.scale.opened = false;
-    // });
-  }
-
-  handleRemoveWorker() {
-    const params = {'node': this.removeWorker.worker};
-    this.operaterService.executeOperate(this.currentCluster.name, 'remove-worker', params).subscribe(() => {
-      this.redirect('deploy');
-    }, error => {
-      this.scale.opened = false;
-    });
-  }
-
-  redirect(url: string) {
-    if (url) {
-      const linkUrl = ['cluster', this.currentCluster.name, url];
-      this.router.navigate(linkUrl);
+    constructor(private nodeService: NodeService, private clusterHealthService: ClusterHealthService,
+                private router: Router, private operaterService: OperaterService, private clusterService: ClusterService,
+                private sessionService: SessionService) {
     }
-  }
 
-  onScale() {
-    this.scale.worker_size = this.workers.length;
-    this.scale.opened = true;
-  }
+    ngOnInit() {
+        this.permission = this.sessionService.getItemPermission(this.currentCluster.item_name);
+        this.nodeService.listNodes(this.currentCluster.name).subscribe(data => {
+            this.workers = data.filter((node) => {
+                return node.roles.includes('worker');
+            });
+        });
+        this.getClusterStatus();
+    }
 
-  onAddWorker() {
-    this.addWorker.loadHosts();
-    this.addWorker.opened = true;
-  }
+    handleScale() {
+        const params = {'num': this.scale.worker_size};
+        this.operaterService.executeOperate(this.currentCluster.name, 'scale', params).subscribe(() => {
+            this.redirect('deploy');
+        }, error => {
+            this.scale.opened = false;
+        });
+    }
 
-  onRemoveWorker() {
-    this.removeWorker.loadNodes(this.currentCluster.name);
-    this.removeWorker.opened = true;
-  }
+    refresh() {
+        this.clusterService.getCluster(this.currentCluster.name).subscribe(data => {
+            this.currentCluster = data;
+        });
+    }
 
-  toHealth() {
-    this.redirect('health');
-  }
+    handleAddWorker() {
+        const hosts = [];
+        this.addWorker.host_names.forEach(h => {
+            hosts.push(h['value']);
+        });
+        const params = {'hosts': hosts};
+        this.operaterService.executeOperate(this.currentCluster.name, 'add-worker', params).subscribe(() => {
+            this.redirect('deploy');
+        }, error => {
+            this.scale.opened = false;
+        });
+    }
 
-  getClusterStatus() {
-    this.loading = true;
-    this.clusterHealthService.listComponent(this.currentCluster.name).subscribe(res => {
-      this.componentData = res;
-      this.loading = false;
-    }, error1 => {
-      this.loading = false;
-    });
-  }
+    handleRemoveWorker() {
+        const params = {'node': this.removeWorker.worker};
+        this.operaterService.executeOperate(this.currentCluster.name, 'remove-worker', params).subscribe(() => {
+            this.redirect('deploy');
+        }, error => {
+            this.scale.opened = false;
+        });
+    }
+
+    redirect(url: string) {
+        if (url) {
+            const linkUrl = ['cluster', this.currentCluster.name, url];
+            this.router.navigate(linkUrl);
+        }
+    }
+
+    onScale() {
+        this.scale.worker_size = this.workers.length;
+        this.scale.opened = true;
+    }
+
+    onAddWorker() {
+        this.addWorker.loadHosts();
+        this.addWorker.opened = true;
+    }
+
+    onRemoveWorker() {
+        this.removeWorker.loadNodes(this.currentCluster.name);
+        this.removeWorker.opened = true;
+    }
+
+    toHealth() {
+        this.redirect('health');
+    }
+
+    getClusterStatus() {
+        this.loading = true;
+        this.clusterHealthService.listComponent(this.currentCluster.name).subscribe(res => {
+            this.componentData = res;
+            this.loading = false;
+        }, error1 => {
+            this.loading = false;
+        });
+    }
 
 
 }
