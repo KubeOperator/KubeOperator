f86fe133a1dd3d9b5f5846d002265c14371a2535
chenyang <chenyang@fit2cloud.com>
2020-04-01 14:37:13 +0800

    添加导入主机模版

--- core/apps/kubeops_api/apis/host.py
+++ core/apps/kubeops_api/apis/host.py
@@ -1,8 +1,10 @@
 import os
 from http import HTTPStatus
 
-from django.http import JsonResponse
-from rest_framework.generics import get_object_or_404, CreateAPIView
+from django.conf import settings
+from django.http import JsonResponse, HttpResponse
+from openpyxl import Workbook
+from rest_framework.generics import get_object_or_404, CreateAPIView, RetrieveAPIView
 from kubeoperator.settings import MEDIA_DIR
 from kubeops_api.host_import import HostImporter
 from kubeops_api.mixin import PageModelViewSet
@@ -22,6 +24,40 @@ class HostImportAPIView(CreateAPIView):
             importer = HostImporter(path=os.path.join(MEDIA_DIR, item))
             importer.run()
         return JsonResponse(data={"success": True}, status=HTTPStatus.CREATED)
+
+
+class DownloadHostImportTemplate(RetrieveAPIView):
+
+    def retrieve(self, request, *args, **kwargs):
+        def create_template_wb():
+            wb = Workbook()
+            s = wb.create_sheet(index=0, title="sheet1")
+            s.cell(1, 1, "name")
+            s.cell(1, 2, "ip")
+            s.cell(1, 3, "port")
+            s.cell(1, 4, "credential")
+            return wb
+
+        def file_iterator(file_name, chunk_size=512):
+            with open(file_name, 'rb') as file:
+                while True:
+                    c = file.read(chunk_size)
+                    if c:
+                        yield c
+                    else:
+                        break
+
+        media_dir = settings.MEDIA_DIR
+        template_path = os.path.join(media_dir, "example_host.xlsx")
+        if not os.path.exists(media_dir):
+            os.mkdir(media_dir)
+        if not os.path.exists(template_path):
+            w = create_template_wb()
+            w.save(template_path)
+        response = HttpResponse(file_iterator(template_path))
+        response["content_type"] = 'application/octet-stream'
+        response['Content-Disposition'] = "attachment; filename=example_host.xlsx"
+        return response
 
 
 class HostViewSet(PageModelViewSet):
