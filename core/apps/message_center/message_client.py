0762fb06c34cd68f32c39118c8bb43081ef6e608
wangzhengkun <zhengkun@fit2cloud.com>
2020-04-02 14:54:31 +0800

    企业微信增加消息重试

--- core/apps/message_center/message_client.py
+++ core/apps/message_center/message_client.py
@@ -44,15 +44,18 @@ class MessageClient():
         messageReceivers = []
         setting_email_enable = False
         email_receivers = ''
-        if len(Setting.objects.filter(key='SMTP_STATUS')) > 0 and Setting.objects.get(key='SMTP_STATUS').value == 'ENABLE':
+        if len(Setting.objects.filter(key='SMTP_STATUS')) > 0 and Setting.objects.get(
+                key='SMTP_STATUS').value == 'ENABLE':
             setting_email_enable = True
         send_ding_talk_enable = False
         ding_talk_receivers = ''
-        if len(Setting.objects.filter(key='DINGTALK_STATUS')) > 0 and Setting.objects.get(key='DINGTALK_STATUS').value == 'ENABLE':
+        if len(Setting.objects.filter(key='DINGTALK_STATUS')) > 0 and Setting.objects.get(
+                key='DINGTALK_STATUS').value == 'ENABLE':
             send_ding_talk_enable = True
         send_weixin_enable = False
         weixin_receivers = ''
-        if len(Setting.objects.filter(key='WEIXIN_STATUS')) > 0 and Setting.objects.get(key='WEIXIN_STATUS').value == 'ENABLE':
+        if len(Setting.objects.filter(key='WEIXIN_STATUS')) > 0 and Setting.objects.get(
+                key='WEIXIN_STATUS').value == 'ENABLE':
             send_weixin_enable = True
 
         for receiver in receivers:
@@ -191,12 +194,18 @@ def send_work_weixin_msg(user_message):
                         agent_id=workWeixin['WEIXIN_AGENT_ID'])
     text = get_msg_content(user_message)
     content = {'content': text}
-    token = get_work_weixin_token()
+    token = get_work_weixin_token(False)
 
     res = weixin.send_markdown_msg(receivers=user_message.receive, content=content, token=token)
     if res.success:
         user_message.receive_status = UserMessage.MESSAGE_RECEIVE_STATUS_SUCCESS
         user_message.save()
+    elif res.data['errcode'] == 40014:
+        token = get_work_weixin_token(True)
+        res = weixin.send_markdown_msg(receivers=user_message.receive, content=content, token=token)
+        if res.success:
+            user_message.receive_status = UserMessage.MESSAGE_RECEIVE_STATUS_SUCCESS
+            user_message.save()
     else:
         logger.error(msg="send workweixin error message_id=" + str(user_message.message_id) + "reason:" + str(res.data),
                      exc_info=True)
@@ -237,17 +246,17 @@ def get_msg_content(user_message):
     return text
 
 
-def get_work_weixin_token():
+def get_work_weixin_token(force):
     redis_cli = redis.StrictRedis(host=kubeoperator.settings.REDIS_HOST, port=kubeoperator.settings.REDIS_PORT)
-    if redis_cli.exists('WORK_WEIXIN_TOKEN'):
-        return redis_cli.get('WORK_WEIXIN_TOKEN')
-    else:
+    if redis_cli.exists('WORK_WEIXIN_TOKEN') == False or force == True:
         workWeixin = Setting.get_settings("workWeixin")
         weixin = WorkWeiXin(corp_id=workWeixin['WEIXIN_CORP_ID'], corp_secret=workWeixin['WEIXIN_CORP_SECRET'],
                             agent_id=workWeixin['WEIXIN_AGENT_ID'])
         result = weixin.get_token()
         redis_cli.set('WORK_WEIXIN_TOKEN', result.data['access_token'], result.data['expires_in'])
         return result.data['access_token']
+    else:
+        return redis_cli.get('WORK_WEIXIN_TOKEN')
 
 
 class MessageReceiver():
