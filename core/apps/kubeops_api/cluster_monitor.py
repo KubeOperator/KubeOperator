406d206a899cc5d8da627c319182237c011e5809
wangzhengkun <zhengkun@fit2cloud.com>
2020-04-24 14:17:44 +0800

    fix bug

--- core/apps/kubeops_api/cluster_monitor.py
+++ core/apps/kubeops_api/cluster_monitor.py
@@ -613,10 +613,11 @@ def delete_unused_node(cluster):
                 if delete_name == node['name']:
                     exist = True
             if exist is False and delete_name != '':
-                C_Host.objects.filter(name=delete_name).delete()
-                if host.groups.filter(name='worker'):
+                if host.groups.filter(name='worker') > 0:
                     cluster.worker_size = cluster.worker_size - 1
                     cluster.save()
+                C_Host.objects.filter(name=delete_name).delete()
+
     return True
 
 
