cfb5eb943505e8fac49900b8b9ebaa2d54dd55c3
wangzhengkun <zhengkun@fit2cloud.com>
2020-05-11 14:29:15 +0800

    参数隐藏

--- ui/src/app/shared/pipe/keys.pipe.ts
+++ ui/src/app/shared/pipe/keys.pipe.ts
@@ -3,12 +3,15 @@ import {Pipe, PipeTransform} from '@angular/core';
 @Pipe({name: 'keys'})
 export class KeysPipe implements PipeTransform {
   transform(obj: Object, args: any[] = null): any {
+    const ingore_keys = ['VC_USERNAME', 'VC_PASSWORD'];
     const array = [];
     Object.keys(obj).forEach(key => {
-      array.push({
-        value: obj[key],
-        key: key
-      });
+      if (ingore_keys.indexOf(key) === -1) {
+        array.push({
+          value: obj[key],
+          key: key
+        });
+      }
     });
     return array;
   }
