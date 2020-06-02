72957f25e05d8c5489f9a2d9bc0e4bda25ca8a1d
wangzhengkun <zhengkun@fit2cloud.com>
2020-06-02 10:46:55 +0800

    host 页面增加 modal alert

--- ui/src/app/business/host/host-create/host-create.component.ts
+++ ui/src/app/business/host/host-create/host-create.component.ts
@@ -5,6 +5,8 @@ import {HostService} from '../host.service';
 import {NgForm} from '@angular/forms';
 import {CredentialService} from '../../setting/credential/credential.service';
 import {Credential} from '../../setting/credential/credential';
+import {ModalAlertService} from '../../../shared/common-component/modal-alert/modal-alert.service';
+import {AlertLevels} from '../../../shared/common-component/common-alert/alert';
 
 @Component({
     selector: 'app-host-create',
@@ -20,7 +22,8 @@ export class HostCreateComponent extends BaseModelComponent<Host> implements OnI
     @ViewChild('hostForm') hostForm: NgForm;
     @Output() created = new EventEmitter();
 
-    constructor(private hostService: HostService, private credentialService: CredentialService) {
+    constructor(private hostService: HostService, private credentialService: CredentialService,
+                private modalAlertService: ModalAlertService) {
         super(hostService);
     }
 
@@ -46,6 +49,9 @@ export class HostCreateComponent extends BaseModelComponent<Host> implements OnI
             this.opened = false;
             this.isSubmitGoing = false;
             this.created.emit();
+        }, error => {
+            this.isSubmitGoing = false;
+            this.modalAlertService.showAlert(error.error.msg, AlertLevels.ERROR);
         });
     }
 }
--- ui/src/app/business/host/host.module.ts
+++ ui/src/app/business/host/host.module.ts
@@ -5,13 +5,15 @@ import { HostCreateComponent } from './host-create/host-create.component';
 import { HostDeleteComponent } from './host-delete/host-delete.component';
 import {CoreModule} from '../../core/core.module';
 import { HostDetailComponent } from './host-detail/host-detail.component';
+import {SharedModule} from '../../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [HostComponent, HostListComponent, HostCreateComponent, HostDeleteComponent, HostDetailComponent],
-  imports: [
-    CoreModule,
-  ]
+    imports: [
+        CoreModule,
+        SharedModule,
+    ]
 })
 export class HostModule { }
