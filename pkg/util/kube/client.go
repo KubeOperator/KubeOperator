0c3b3f7bc28f49f39b949cf8d0d2c4876af3bbcb
wangzhengkun <zhengkun@fit2cloud.com>
2020-06-05 18:20:57 +0800

    增加 k8s client

---
+++ pkg/util/kube/client.go
@@ -0,0 +1,79 @@
+package kube
+
+import (
+	"context"
+	v1 "k8s.io/api/core/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+type Interface interface {
+	getPodsByNamespace(namespace string) ([]v1.Pod, error)
+	getAllPods() ([]v1.Pod, error)
+	getNameSpaces() ([]v1.Namespace, error)
+	GetEvents() ([]v1.Event, error)
+}
+
+type Kube struct {
+	config *KubeConfig
+	client kubernetes.Clientset
+}
+
+func NewKube(k *KubeConfig) (*Kube, error) {
+	client, err := k.InitK8SClient()
+	if err != nil {
+		return nil, err
+	}
+	return &Kube{
+		config: k,
+		client: *client,
+	}, nil
+}
+
+func (k *Kube) getNameSpaces() ([]v1.Namespace, error) {
+	namespaceList, err := k.client.CoreV1().Namespaces().List(context.TODO(), metaV1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return namespaceList.Items, err
+}
+
+func (k *Kube) getPodsByNamespace(namespace string) ([]v1.Pod, error) {
+	podList, err := k.client.CoreV1().Pods(namespace).List(context.TODO(), metaV1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return podList.Items, err
+}
+
+func (k *Kube) getAllPods() ([]v1.Pod, error) {
+	var pods []v1.Pod
+	namespaceList, err := k.client.CoreV1().Namespaces().List(context.TODO(), metaV1.ListOptions{})
+	if err != nil {
+		return pods, err
+	}
+	for _, namespace := range namespaceList.Items {
+		podList, err := k.client.CoreV1().Pods(namespace.Name).List(context.TODO(), metaV1.ListOptions{})
+		if err != nil {
+			break
+		}
+		pods = append(pods, podList.Items...)
+	}
+	return pods, err
+}
+
+func (k *Kube) GetEvents() ([]v1.Event, error) {
+	var events []v1.Event
+	namespaceList, err := k.client.CoreV1().Namespaces().List(context.TODO(), metaV1.ListOptions{})
+	if err != nil {
+		return events, err
+	}
+	for _, namespace := range namespaceList.Items {
+		eventList, err := k.client.CoreV1().Events(namespace.Name).List(context.TODO(), metaV1.ListOptions{})
+		if err != nil {
+			break
+		}
+		events = append(events, eventList.Items...)
+	}
+	return events, err
+}
