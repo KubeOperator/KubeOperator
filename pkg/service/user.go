5aada94fa3ded81810698086496ad6e947060c8c
wangzhengkun <zhengkun@fit2cloud.com>
2020-06-11 11:05:25 +0800

    后端国际化

--- pkg/service/user.go
+++ pkg/service/user.go
@@ -4,19 +4,24 @@ import (
 	"errors"
 	"github.com/KubeOperator/KubeOperator/pkg/auth"
 	"github.com/KubeOperator/KubeOperator/pkg/db"
+	"github.com/KubeOperator/KubeOperator/pkg/i18n"
 	"github.com/KubeOperator/KubeOperator/pkg/model/user"
 	"github.com/KubeOperator/KubeOperator/pkg/util/encrypt"
 )
 
 var (
-	UserNotFound     = errors.New("can not find user")
-	PasswordNotMatch = errors.New("password not match")
+	UserNotFound     = errors.New(i18n.Tr("user_not_found", nil))
+	PasswordNotMatch = errors.New(i18n.Tr("password_not_match", nil))
+	UserIsNotActive  = errors.New(i18n.Tr("user_is_not_active", nil))
 )
 
 func UserAuth(name string, password string) (sessionUser *auth.SessionUser, err error) {
 	var dbUser user.User
 	if db.DB.Where("name = ?", name).First(&dbUser).RecordNotFound() {
 		return nil, UserNotFound
+	}
+	if dbUser.IsActive == false {
+		return nil, UserIsNotActive
 	}
 	password, err = encrypt.StringEncrypt(password)
 	if err != nil {
