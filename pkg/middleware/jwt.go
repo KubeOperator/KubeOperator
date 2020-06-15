0585ad24166718ce7b1fa2707994f06f921522df
zhengkunwang223 <zhengkun@fit2cloud.com>
2020-06-15 23:05:29 +0800

    i18n

--- pkg/middleware/jwt.go
+++ pkg/middleware/jwt.go
@@ -16,11 +16,6 @@ var (
 	exp       int64
 )
 
-//func GetJwtConfig()  {
-//	v := viper.New()
-//
-//}
-
 func JWTMiddleware() *jwtmiddleware.Middleware {
 	secretKey = []byte(viper.GetString("jwt.secret"))
 	exp = viper.GetInt64("jwt.exp")
@@ -30,11 +25,20 @@ func JWTMiddleware() *jwtmiddleware.Middleware {
 			return secretKey, nil
 		},
 		SigningMethod: jwt.SigningMethodHS256,
+		ErrorHandler:  ErrorHandler,
 	})
 }
 
-func ErrorHandler() {
-
+func ErrorHandler(ctx context.Context, err error) {
+	if err == nil {
+		return
+	}
+	ctx.StopExecution()
+	response := &dto.Response{
+		Status: false,
+		Msg:    err.Error(),
+	}
+	ctx.JSON(response)
 }
 
 func LoginHandler(ctx context.Context) {
@@ -84,67 +88,3 @@ func CreateToken(user *auth.SessionUser) (string, error) {
 	}
 	return tokenString, err
 }
-
-//func JWTMiddleware() *jwt.GinJWTMiddleware {
-//	secret := viper.GetString("app.secret")
-//	j, err := jwt.New(&jwt.GinJWTMiddleware{
-//		Key:           []byte(secret),
-//		Timeout:       time.Hour,
-//		MaxRefresh:    time.Hour,
-//		TimeFunc:      time.Now,
-//		TokenHeadName: "Bearer",
-//		IdentityKey:   "user",
-//		Authenticator: func(ctx *gin.Context) (i interface{}, err error) {
-//			var credential auth.Credential
-//			if err := ctx.ShouldBind(&credential); err != nil {
-//				return nil, jwt.ErrMissingLoginValues
-//			}
-//			sUser, err := service.UserAuth(credential.Username, credential.Password)
-//			if err != nil {
-//				if sUser != nil && sUser.IsActive == false {
-//					return nil, err
-//				} else {
-//					return nil, err
-//				}
-//			}
-//			return sUser, nil
-//		},
-//		PayloadFunc: func(data interface{}) jwt.MapClaims {
-//			if v, ok := data.(*auth.SessionUser); ok {
-//				sessionUser = *v
-//				return jwt.MapClaims{
-//					"user": v,
-//				}
-//			}
-//			return jwt.MapClaims{}
-//		},
-//		IdentityHandler: func(ctx *gin.Context) interface{} {
-//			claims := jwt.ExtractClaims(ctx)
-//			return &claims
-//		},
-//		LoginResponse: func(ctx *gin.Context, code int, token string, expire time.Time) {
-//			ctx.JSON(http.StatusOK, gin.H{
-//				"code":   code,
-//				"token":  token,
-//				"expire": expire.Format(time.RFC3339),
-//				"user":   sessionUser,
-//			})
-//			return
-//		},
-//	})
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	return j
-//}
-//
-//func GetAuthUser(ctx *gin.Context) {
-//	claims := jwt.ExtractClaims(ctx)
-//	token := ctx.Keys["JWT_TOKEN"].(string)
-//	ctx.JSON(http.StatusOK, gin.H{
-//		"code":  http.StatusOK,
-//		"token": token,
-//		"user":  claims["user"],
-//	})
-//	return
-//}
--- pkg/router/root.go
+++ pkg/router/root.go
@@ -9,6 +9,8 @@ import (
 
 func Server() *iris.Application {
 	app := iris.New()
+	_ = app.I18n.Load("../pkg/locales/*/*", "en-US", "zh-CN")
+	app.I18n.SetDefault("zh-CN")
 	app.Post("/api/auth/login", middleware.LoginHandler)
 	app.Use(middleware.LogMiddleware)
 	app.Use(middleware.JWTMiddleware().Serve)
