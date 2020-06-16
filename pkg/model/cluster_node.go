f652f9b5f72e67ead04b4ead5fe05f2e992a294a
chenyang <chenyang@fit2cloud.com>
2020-06-16 16:59:01 +0800

    change pkg

--- pkg/model/cluster_node.go
+++ pkg/model/cluster_node.go
@@ -13,11 +13,11 @@ import (
 
 type ClusterNode struct {
 	common.BaseModel
-	ID        string         `json:"_"`
-	Name      string         `gorm:"not null;unique" json:"name"`
-	Host      hostModel.Host `gorm:"save_associations:false"`
-	ClusterID string         `json:"clusterId"`
-	Role      string         `json:"role"`
+	ID        string     `json:"_"`
+	Name      string     `gorm:"not null;unique" json:"name"`
+	Host      Host `gorm:"save_associations:false"`
+	ClusterID string     `json:"clusterId"`
+	Role      string     `json:"role"`
 }
 
 func (n *ClusterNode) BeforeCreate() (err error) {
