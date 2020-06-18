f2a94f40f107fbc09aa549bb31826ad8d22961bf
chenyang <chenyang@fit2cloud.com>
2020-06-18 14:40:45 +0800

    warp controller error

---
+++ pkg/controller/errors/errors.go
@@ -0,0 +1,23 @@
+package errors
+
+import "encoding/json"
+
+type ControllerError struct {
+	err error
+}
+
+func NewControllerError(err error) error {
+	return &ControllerError{err: err}
+}
+
+func (e *ControllerError) Error() string {
+	warp := struct {
+		Msg string `json:"msg"`
+	}{e.err.Error(),}
+	b, _ := json.Marshal(&warp)
+	return string(b)
+}
+
+func (e *ControllerError) Unwrap() error {
+	return e.err
+}
