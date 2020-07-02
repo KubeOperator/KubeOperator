bf57cd1f3686d33d1014efe17295c3abf5bc2a54
chenyang <chenyang@fit2cloud.com>
2020-07-02 19:19:13 +0800

    add create provider

--- pkg/service/cluster/adm/nfs.go
+++ pkg/service/cluster/adm/nfs.go
@@ -1,1 +1,13 @@
 package adm
+
+import "time"
+
+func (ca *ClusterAdm) EnsureDeployNfsTaskStart(c *Cluster) error {
+	time.Sleep(1 * time.Second)
+	return nil
+}
+
+func (ca *ClusterAdm) EnsureDeployNfsProvisioner(c *Cluster) error {
+	time.Sleep(1 * time.Second)
+	return nil
+}
--- ui/src/app/business/cluster/cluster-detail/storage/storage-provisioner/storage-provisioner-create/storage-provisioner-create.component.ts
+++ ui/src/app/business/cluster/cluster-detail/storage/storage-provisioner/storage-provisioner-create/storage-provisioner-create.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CreateStorageProvisionerRequest} from '../storage-provisioner';
 
 @Component({
-  selector: 'app-storage-provisioner-create',
-  templateUrl: './storage-provisioner-create.component.html',
-  styleUrls: ['./storage-provisioner-create.component.css']
+    selector: 'app-storage-provisioner-create',
+    templateUrl: './storage-provisioner-create.component.html',
+    styleUrls: ['./storage-provisioner-create.component.css']
 })
 export class StorageProvisionerCreateComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    opened = false;
+    item = new CreateStorageProvisionerRequest();
+
+    ngOnInit(): void {
+    }
+
 
 }
