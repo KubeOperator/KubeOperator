05fadbb6cbd1c834cc36a1ba5d332ac1f4ba50a8
wangzhengkun <zhengkun@fit2cloud.com>
2020-07-13 15:20:54 +0800

    add example

--- pkg/util/kotf/kotf_test.go
+++ pkg/util/kotf/kotf_test.go
@@ -10,6 +10,7 @@ func TestKotfIint(t *testing.T) {
 		Cluster: "clsuter3",
 	})
 
+	// vsphere example
 	provider := `{
     "name": "vSphere",
     "username": "",
@@ -50,6 +51,37 @@ func TestKotfIint(t *testing.T) {
       }
     }
   ]`
+
+	// openstack example
+
+	//provider := `{
+	//  "username": "admin",
+	//  "password": "",
+	//  "identity": "",
+	//  "projectId": "",
+	//  "domainName": ""
+	//}`
+
+	//cloudRegion := `{
+	//  "datacenter": "RegionOne",
+	//}`
+
+	//hosts := `[
+	//  {
+	//    "shortName": "worker2",
+	//    "name": "worker2.clsuter3.fit2cloud.com",
+	//    "ip": "172.16.10.223",
+	//    "model": "m1.small",
+	//    "zone": {
+	//      "name": "nova",
+	//      "network": "fd989e3e-3d49-4658-b852-b65bbd552220",
+	//      "imageName": "cirros",
+	//      "securityGroup": "default",
+	//      "subnet": "a6019a57-2a14-4551-8331-46822b3d3981",
+	//      "ipType": "private"
+	//    }
+	//  }
+	//]`
 
 	test, err := terraform.Init("vSphere", provider, cloudRegion, hosts)
 	if err != nil {
