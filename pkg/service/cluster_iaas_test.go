f483dc93187896a9ca79b6cf6072f3d4cbc70322
chenyang <907693148@qq.com>
2020-07-14 16:30:49 +0800

    feat(cluster) 集群卸载

--- pkg/service/cluster_iaas_test.go
+++ pkg/service/cluster_iaas_test.go
@@ -1,1 +1,47 @@
-package cluster
+package service
+
+import (
+	"github.com/KubeOperator/KubeOperator/pkg/config"
+	"github.com/KubeOperator/KubeOperator/pkg/db"
+	"github.com/KubeOperator/KubeOperator/pkg/dto"
+	"github.com/spf13/viper"
+	"testing"
+)
+
+func Init() {
+	config.Init()
+	phase := db.InitDBPhase{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetInt("db.port"),
+		Name:     viper.GetString("db.name"),
+		User:     viper.GetString("db.user"),
+		Password: viper.GetString("db.password"),
+	}
+	err := phase.Init()
+	if err != nil {
+		log.Fatalf("can not init db,%s", err)
+	}
+}
+
+func TestClusterIaasService_Init(t *testing.T) {
+	Init()
+	s := NewClusterService()
+
+	s.Create(dto.ClusterCreate{
+		Name:                 "bbb",
+		Version:              "",
+		Provider:             "plan",
+		Plan:                 "",
+		WorkerAmount:         "",
+		NetworkType:          "",
+		RuntimeType:          "",
+		DockerStorageDIr:     "",
+		ContainerdStorageDIr: "",
+		FlannelBackend:       "",
+		CalicoIpv4poolIpip:   "",
+		KubePodSubnet:        "",
+		KubeServiceSubnet:    "",
+	})
+
+
+}
