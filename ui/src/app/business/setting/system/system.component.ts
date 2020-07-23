4adbfd10fde6d689ac70421f0f048f514e76cc28
wangzhengkun <zhengkun@fit2cloud.com>
2020-07-23 18:27:43 +0800

    fix(system): 系统设置增加IP校验

--- ui/src/app/business/setting/system/system.component.ts
+++ ui/src/app/business/setting/system/system.component.ts
@@ -5,6 +5,8 @@ import {SystemService} from '../system.service';
 import {AlertLevels} from '../../../layout/common-alert/alert';
 import {CommonAlertService} from '../../../layout/common-alert/common-alert.service';
 import {TranslateService} from '@ngx-translate/core';
+import * as ipaddr from 'ipaddr.js';
+
 
 @Component({
     selector: 'app-system',
@@ -34,10 +36,27 @@ export class SystemComponent extends BaseModelComponent<System> implements OnIni
     }
 
     onSubmit() {
+        if (!this.checkIp()) {
+            return;
+        }
         this.systemService.create(this.item).subscribe(res => {
             this.commonAlertService.showAlert(this.translateService.instant('APP_ADD_SUCCESS'), AlertLevels.SUCCESS);
         }, error => {
-            this.commonAlertService.showAlert(error.error.msg, AlertLevels.SUCCESS);
+            this.commonAlertService.showAlert(error.error.msg, AlertLevels.ERROR);
         });
+    }
+
+    checkIp() {
+        const ip = this.item.vars['ip'];
+        if (!ipaddr.isValid(ip)) {
+            this.commonAlertService.showAlert(this.translateService.instant('APP_IP_INVALID'), AlertLevels.ERROR);
+            return false;
+        }
+        const ntp = this.item.vars['ntp_server'];
+        if (ntp !== '' && !ipaddr.isValid(ntp)) {
+            this.commonAlertService.showAlert(this.translateService.instant('APP_NTP_INVALID'), AlertLevels.ERROR);
+            return false;
+        }
+        return true;
     }
 }
