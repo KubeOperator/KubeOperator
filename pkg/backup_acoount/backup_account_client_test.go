018419eeba5d0174da17810a318a00973ac3ca90
wangzhengkun <zhengkun@fit2cloud.com>
2020-08-05 14:55:16 +0800

    feat(storage client): 撤销文件

--- pkg/backup_acoount/backup_account_client_test.go
+++ pkg/backup_acoount/backup_account_client_test.go
@@ -7,24 +7,24 @@ import (
 
 func Test(t *testing.T) {
 	vars := make(map[string]string)
-	//vars["type"] = "OSS"
-	//vars["endpoint"] = "http://oss-cn-hangzhou.aliyuncs.com"
-	//vars["accessKey"] = "LTAI4Fd1E5YrrhsBjf7zrEBp"
-	//vars["secretKey"] = "Z9iGtDe3qK4VUlBSHuAV9XvCcQ3r6c"
-	//vars["bucket"]  = "kube-operator"
-
-	//vars["type"] = "AZURE"
-	//vars["endpoint"] = "blob.core.chinacloudapi.cn"
-	//vars["accountName"] = "zhengkun"
-	//vars["accountKey"] = "UEcy3YKS46t/BXsKm8vAOY3tLkll1+EmJzr9GMekz8tniCJybmFnqVWAoC9WvlTzvPnz9rpfts8J2ma7nemQSg=="
-	//vars["bucket"] = "test"
-
-	vars["type"] = "S3"
-	vars["endpoint"] = "http://s3.cn-north-1.amazonaws.com.cn"
-	vars["accessKey"] = "AKIAWZUDY4HG4JJH4TXN"
-	vars["secretKey"] = "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"
-	vars["region"] = "cn-north-1"
-	vars["bucket"] = "kube-operator"
+	////vars["type"] = "OSS"
+	////vars["endpoint"] = "http://oss-cn-hangzhou.aliyuncs.com"
+	////vars["accessKey"] = "LTAI4Fd1E5YrrhsBjf7zrEBp"
+	////vars["secretKey"] = "Z9iGtDe3qK4VUlBSHuAV9XvCcQ3r6c"
+	////vars["bucket"]  = "kube-operator"
+	//
+	////vars["type"] = "AZURE"
+	////vars["endpoint"] = "blob.core.chinacloudapi.cn"
+	////vars["accountName"] = "zhengkun"
+	////vars["accountKey"] = "UEcy3YKS46t/BXsKm8vAOY3tLkll1+EmJzr9GMekz8tniCJybmFnqVWAoC9WvlTzvPnz9rpfts8J2ma7nemQSg=="
+	////vars["bucket"] = "test"
+	//
+	//vars["type"] = "S3"
+	//vars["endpoint"] = "http://s3.cn-north-1.amazonaws.com.cn"
+	//vars["accessKey"] = "AKIAWZUDY4HG4JJH4TXN"
+	//vars["secretKey"] = "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"
+	//vars["region"] = "cn-north-1"
+	//vars["bucket"] = "kube-operator"
 	client, err := NewBackupAccountClient(vars)
 	if err != nil {
 		fmt.Println(err)
