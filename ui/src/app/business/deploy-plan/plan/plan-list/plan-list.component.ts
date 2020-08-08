7770f5da8759f9fa6dd04d57279e9e05e0891edd
zhengkunwang223 <zhengkun@fit2cloud.com>
2020-08-08 17:32:39 +0800

    fix(plan): 增加部署计划展示信息

--- ui/src/app/business/deploy-plan/plan/plan-list/plan-list.component.ts
+++ ui/src/app/business/deploy-plan/plan/plan-list/plan-list.component.ts
@@ -2,6 +2,7 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {BaseModelComponent} from '../../../../shared/class/BaseModelComponent';
 import {Plan} from '../plan';
 import {PlanService} from '../plan.service';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
     selector: 'app-plan-list',
@@ -14,7 +15,7 @@ export class PlanListComponent extends BaseModelComponent<Plan> implements OnIni
     @Output() detailEvent = new EventEmitter<Plan>();
 
 
-    constructor(private planService: PlanService) {
+    constructor(private planService: PlanService, private translateService: TranslateService) {
         super(planService);
     }
 
@@ -26,4 +27,14 @@ export class PlanListComponent extends BaseModelComponent<Plan> implements OnIni
         this.detailEvent.emit(item);
     }
 
+    getDeployName(name: string) {
+        switch (name) {
+            case 'SINGLE':
+                return this.translateService.instant('APP_PLAN_DEPLOY_TEMPLATE_SINGLE');
+            case 'MULTIPLE':
+                return this.translateService.instant('APP_PLAN_DEPLOY_TEMPLATE_MULTIPLE');
+            default:
+                return 'None';
+        }
+    }
 }
