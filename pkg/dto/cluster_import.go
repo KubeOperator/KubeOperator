519b11d95a8adecfb6559c6080ce57fd42d9d4a1
chenyang <907693148@qq.com>
2020-08-17 17:43:22 +0800

    feat(cluster) 开放导入集群功能

---
+++ pkg/dto/cluster_import.go
@@ -0,0 +1,9 @@
+package dto
+
+type ClusterImport struct {
+	Name        string `json:"name"`
+	ApiServer   string `json:"apiServer"`
+	Router      string `json:"router"`
+	Token       string `json:"token"`
+	ProjectName string `json:"projectName"`
+}
