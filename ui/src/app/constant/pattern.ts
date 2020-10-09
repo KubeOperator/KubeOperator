6629364423ac4da912a91d02816b5e2f78239f46
chenyang <907693148@qq.com>
2020-10-09 18:43:58 +0800

    fix(plan): 所有plan 下的资源名称都不能以数字开头

--- ui/src/app/constant/pattern.ts
+++ ui/src/app/constant/pattern.ts
@@ -1,3 +1,3 @@
 export const PasswordPattern = '^(?=.*\\d)(?=.*[a-zA-Z])[\\da-zA-Z~!@#$%^&*]{6,30}$';
-export const NamePattern = '^[\u4E00-\u9FA5A-Za-z0-9_-]+$';
-export const NamePatternHelper = '支持中文英文数字、下划线';
+export const NamePattern = '^[a-zA-Z\u4e00-\u9fa5]{1}[a-zA-Z0-9_\u4e00-\u9fa5]{0,30}$';
+export const NamePatternHelper = '支持中文英文数字、下划线,且不以数字开头';
