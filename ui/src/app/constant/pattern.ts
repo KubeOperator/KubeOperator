427cc92894bea03af6871f46414b50ea1cdf5dbd
wangzhengkun <zhengkun@fit2cloud.com>
2020-10-14 15:08:13 +0800

    feat(vmConfig): 虚拟机配置名称增加限制

--- ui/src/app/constant/pattern.ts
+++ ui/src/app/constant/pattern.ts
@@ -1,3 +1,6 @@
 export const PasswordPattern = '^(?=.*\\d)(?=.*[a-zA-Z])[\\da-zA-Z~!@#$%^&*]{6,30}$';
 export const NamePattern = '^[a-zA-Z\u4e00-\u9fa5]{1}[a-zA-Z0-9_\u4e00-\u9fa5]{0,30}$';
 export const NamePatternHelper = '支持中文英文数字、下划线,且不以数字开头';
+export const ConfigPattern = '^[a-zA-Z]{1}[a-zA-Z0-9]{0,30}$';
+export const ConfigPatternHelper = '支持英文和数字,且不以数字开头';
+
