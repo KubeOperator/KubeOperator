5fe0f6bcb949997fd6c675d8d83420393883c852
xiaohu <xiaohu.lv@fit2cloud.com>
2020-10-21 16:32:18 +0800

    feat: 添加F5数据库查询

--- pkg/repository/f5.go
+++ pkg/repository/f5.go
@@ -7,7 +7,7 @@ import (
 
 type F5Repository interface {
 	Save(plan *model.F5Setting) error
-	//Get(name string) (model.F5Setting,error)
+	Get(name string) ([]model.F5Setting, error)
 }
 
 func NewF5Repository() F5Repository {
@@ -19,36 +19,22 @@ type f5Repository struct {
 
 func (f f5Repository) Save(f5 *model.F5Setting) error {
 	if db.DB.NewRecord(f5) {
-		tx := db.DB.Begin()
-		err := tx.Create(&f5).Error
-		if err != nil {
-			tx.Rollback()
-			return err
-		}
-		tx.Commit()
-		return err
+		return db.DB.Create(&f5).Error
 	} else {
-		tx := db.DB.Begin()
-		err := tx.Create(&f5).Error
-		if err != nil {
-			tx.Rollback()
-			return err
-		}
-		err = tx.Where("id = ?", f5.ID).Delete(&model.F5Setting{}).Error
-		if err != nil {
-			tx.Rollback()
-			return err
-		}
-		tx.Commit()
-		return err
+		return db.DB.Model(&f5).Update(&f5).Error
 	}
 }
 
-// 未完成
-//func (f f5Repository) Get(name *model.F5Setting) (model.F5Setting,error)  {
-//	var f5 model.F5Setting
-//	if err  := db.DB.Where(f5).First(&f5).Error; err != nil {
-//		return f5,err
-//	}
-//	return f5,nil
-//}
+func (f f5Repository) Get(clusterName string) ([]model.F5Setting, error) {
+	var cluster model.Cluster
+	if err := db.DB.Where(model.Cluster{Name: clusterName}).First(&cluster).Error; err != nil {
+		return nil, err
+	}
+	var f5 []model.F5Setting
+	if err := db.DB.
+		Where(model.F5Setting{ClusterID: cluster.ID}).
+		Find(&f5).Error; err != nil {
+		return nil, err
+	}
+	return f5, nil
+}
