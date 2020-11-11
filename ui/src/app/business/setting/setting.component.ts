09db651cb04b0e4e88d04bd1eefe7e46d6c0d16d
wangzhengkun <zhengkun@fit2cloud.com>
2020-11-11 10:58:19 +0800

    fix(license): 回滚代码

--- ui/src/app/business/setting/setting.component.ts
+++ ui/src/app/business/setting/setting.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {BusinessLicenseService} from '../../shared/service/business-license.service';
+import {LicenseService} from './license/license.service';
 
 @Component({
     selector: 'app-setting',
@@ -8,17 +9,17 @@ import {BusinessLicenseService} from '../../shared/service/business-license.serv
 })
 export class SettingComponent implements OnInit {
 
-    constructor(public businessLicenseService: BusinessLicenseService) {
+    constructor(public businessLicenseService: BusinessLicenseService, private licenseService: LicenseService) {
     }
 
     hasLicense = false;
 
     ngOnInit(): void {
-        // this.licenseService.get().subscribe(data => {
-        //     if (data.status === 'valid') {
-        //         this.hasLicense = true;
-        //     }
-        // });
-        this.hasLicense = this.businessLicenseService.licenseValid;
+        this.licenseService.get().subscribe(data => {
+            if (data.status === 'valid') {
+                this.hasLicense = true;
+            }
+        });
+        // this.hasLicense = this.businessLicenseService.licenseValid;
     }
 }
