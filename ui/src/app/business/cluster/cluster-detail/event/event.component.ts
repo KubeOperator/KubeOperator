80d0820c7f5a0da8afcba07bedf214746a1f3b37
wangzhengkun <zhengkun@fit2cloud.com>
2020-11-11 15:09:00 +0800

    fix(event): 启用NPD失败后重置按钮

--- ui/src/app/business/cluster/cluster-detail/event/event.component.ts
+++ ui/src/app/business/cluster/cluster-detail/event/event.component.ts
@@ -77,6 +77,7 @@ export class EventComponent implements OnInit {
         this.eventService.changeNpd(this.currentCluster.name, op).subscribe(res => {
             this.commonAlertService.showAlert(this.translateService.instant('APP_UPDATE_SUCCESS'), AlertLevels.SUCCESS);
         }, error => {
+            this.npdExists = exists;
             this.commonAlertService.showAlert(error.error.msg, AlertLevels.ERROR);
         });
     }
