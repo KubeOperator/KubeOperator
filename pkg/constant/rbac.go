3f5cc3b2a7e25b30d37fbf1eea952b8363bc35ba
wangzhengkun <zhengkun@fit2cloud.com>
2020-11-12 14:45:49 +0800

    feat(event): 修改权限

--- pkg/constant/rbac.go
+++ pkg/constant/rbac.go
@@ -24,6 +24,7 @@ var SystemRules = loader.AdvancedRules{
 			"/api/v1/clusters/{**}",
 			"/api/v1/clusters/{**}/{**}",
 			"/api/v1/clusters/{**}/{**}/{**}",
+			"/api/v1/clusters/{**}/{**}/{**}/{**}",
 			"/api/v1/message/{**}",
 			"/api/v1/events/npd/{**}/{**}",
 			"/api/v1/users/change/password",
