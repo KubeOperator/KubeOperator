8c582cc6616be53119c71a141238229d40d22787
xiaohu <xiaohu.lv@fit2cloud.com>
2020-11-20 14:36:39 +0800

    feat(cluster): 修改集群创建时的默认网络参数

--- ui/src/app/business/cluster/cluster-create/cluster-create.component.ts
+++ ui/src/app/business/cluster/cluster-create/cluster-create.component.ts
@@ -72,9 +72,9 @@ export class ClusterCreateComponent implements OnInit {
         this.item.containerdStorageDir = '/var/lib/containerd';
         this.item.flannelBackend = 'vxlan';
         this.item.calicoIpv4poolIpip = 'Always';
-        this.item.kubePodSubnet = '179.10.0.0/16';
-        this.item.kubeServiceSubnet = '179.20.0.0/16';
-        this.item.dockerSubnet = '179.30.0.1/16';
+        this.item.kubePodSubnet = '10.244.0.0/18';
+        this.item.kubeServiceSubnet = '10.244.64.0/18';
+        this.item.dockerSubnet = '172.17.0.1/16';
         this.item.kubeMaxPods = 110;
         this.item.certsExpired = 36500;
         this.item.kubernetesAudit = 'no';
