9a95df3ef9043a98510a51203b37ad03d343f845
ssongliu <songlius11@163.com>
2020-11-30 16:24:52 +0800

    fix(license, backup,host):  上传样式修改

--- migration/38_add_table_system_log.up.sql
+++ migration/38_add_table_system_log.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `ko_system_log` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `id` varchar(64) NOT NULL,
-  `name` varchar(256) NOT NULL
+  `name` varchar(256) NOT NULL,
   `operation` varchar(256) NOT NULL,
   `operation_info` varchar(256),
   PRIMARY KEY (`id`)
