2af34e7d563a21693fe378824d33eeebea22623b
wangzhengkun <zhengkun@fit2cloud.com>
2021-01-07 22:55:38 +0800

    fix(ip-pool): 删除ip池级联删除ip

--- pkg/service/ip_pool.go
+++ pkg/service/ip_pool.go
@@ -133,7 +133,7 @@ func (i ipPoolService) Batch(op dto.IpPoolOp) error {
 	case constant.BatchOperationDelete:
 		for i := range opItems {
 			var ipPool model.IpPool
-			if err := tx.Where(model.IpPool{Name: opItems[i].Name}).Preload("Ips").First(&ipPool).Error; err != nil {
+			if err := tx.Where(model.IpPool{Name: opItems[i].Name}).First(&ipPool).Error; err != nil {
 				tx.Rollback()
 				return err
 			}
@@ -142,8 +142,10 @@ func (i ipPoolService) Batch(op dto.IpPoolOp) error {
 				return err
 			}
 
-			if len(ipPool.Ips) > 0 {
-				if err := tx.Delete(&ipPool.Ips).Error; err != nil {
+			var ips []model.Ip
+			tx.Where(model.Ip{IpPoolID: ipPool.ID}).Find(&ips)
+			if len(ips) > 0 {
+				if err := tx.Delete(&ips).Error; err != nil {
 					tx.Rollback()
 					return err
 				}
