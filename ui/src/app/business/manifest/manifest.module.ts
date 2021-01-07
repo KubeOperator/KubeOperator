41bbd36ddc97ec970b26268a0e151f65490a2de5
xiaohu <xiaohu.lv@fit2cloud.com>
2021-01-07 16:23:00 +0800

    feat(style) update

--- ui/src/app/business/manifest/manifest.module.ts
+++ ui/src/app/business/manifest/manifest.module.ts
@@ -1,15 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {ManifestComponent} from "./manifest.component";
-import {ManifestListComponent} from "./manifest-list/manifest-list.component";
-import {ManifestDetailComponent} from "./manifest-detail/manifest-detail.component";
-import {CoreModule} from "../../core/core.module";
-import {SharedModule} from "../../shared/shared.module";
+import {ManifestComponent} from './manifest.component';
+import {ManifestListComponent} from './manifest-list/manifest-list.component';
+import {ManifestDetailComponent} from './manifest-detail/manifest-detail.component';
+import {CoreModule} from '../../core/core.module';
+import {SharedModule} from '../../shared/shared.module';
+import { ManifestAlertComponent } from './manifest-alert/manifest-alert.component';
 
 
 @NgModule({
     declarations: [
-        ManifestComponent, ManifestListComponent, ManifestDetailComponent
+        ManifestComponent, ManifestListComponent, ManifestDetailComponent, ManifestAlertComponent
     ],
     imports: [
         CoreModule,
