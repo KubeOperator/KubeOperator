663358e9915347c2e4e4ab92754d8e3d4166922c
wangzhengkun <zhengkun@fit2cloud.com>
2021-01-13 12:16:28 +0800

    fix(email): 修改邮件逻辑

--- ui/src/app/business/setting/email/email.component.ts
+++ ui/src/app/business/setting/email/email.component.ts
@@ -16,25 +16,28 @@ export class EmailComponent implements OnInit {
     item: System = new System();
     valid = false;
     createItem: SystemCreateRequest = new SystemCreateRequest();
+    checking = false;
 
     constructor(private systemService: SystemService, private commonAlertService: CommonAlertService,
                 private translateService: TranslateService) {
     }
 
     ngOnInit(): void {
-        this.systemService.getByTab('EMAIL').subscribe(res => {
-            this.item = res;
-        });
+        this.getSetting();
     }
 
 
     checkValid(type) {
-        this.item.tab = 'EMAIL';
-        this.systemService.checkBy(type, this.item).subscribe(res => {
+        this.createItem.vars = this.item.vars;
+        this.createItem.tab = 'EMAIL';
+        this.checking = true;
+        this.systemService.checkBy(type, this.createItem).subscribe(res => {
             this.valid = true;
+            this.checking = false;
             this.commonAlertService.showAlert(this.translateService.instant('APP_CHECK_SUCCESS'), AlertLevels.SUCCESS);
         }, error => {
             this.valid = false;
+            this.checking = false;
             this.commonAlertService.showAlert(error.error.msg, AlertLevels.ERROR);
         });
     }
@@ -43,9 +46,24 @@ export class EmailComponent implements OnInit {
         this.createItem.vars = this.item.vars;
         this.createItem.tab = 'EMAIL';
         this.systemService.create(this.createItem).subscribe(res => {
-            this.commonAlertService.showAlert(this.translateService.instant('APP_ADD_SUCCESS'), AlertLevels.SUCCESS);
+            let key = 'APP_ADD_SUCCESS';
+            if (this.item !== undefined && this.item.tab !== '') {
+                key = 'APP_UPDATE_SUCCESS';
+            }
+            this.commonAlertService.showAlert(this.translateService.instant(key), AlertLevels.SUCCESS);
+            this.getSetting();
         }, error => {
             this.commonAlertService.showAlert(error.error.msg, AlertLevels.ERROR);
+        });
+    }
+
+    changeStatus(status) {
+        this.valid = status !== 'ENABLE';
+    }
+
+    getSetting() {
+        this.systemService.getByTab('EMAIL').subscribe(res => {
+            this.item = res;
         });
     }
 }
