54c83aab52192bbef1a630610384832ba9a70d17
chenyang <907693148@qq.com>
2021-01-20 15:04:36 +0800

    feat(config): 读取系统环境变量

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,11 +2,27 @@ package config
 
 import (
 	"github.com/spf13/viper"
+	"strings"
 )
+import "os"
 
 func Init() {
 	viper.SetConfigName("app")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath("/etc/ko")
 	_ = viper.ReadInConfig()
+	splitOsEnv()
+}
+
+func splitOsEnv() {
+	for i := range os.Environ() {
+		ks := strings.Split(os.Environ()[i], "=")
+		key := ks[0]
+		value := ks[1]
+		if strings.HasPrefix(key, "KO_") {
+			cfk := strings.Replace(strings.ToLower(strings.Replace(key, "KO_", "", -1)), "_", ".", -1)
+			viper.Set(cfk, value)
+		}
+
+	}
 }
