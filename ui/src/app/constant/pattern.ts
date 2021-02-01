6f8cfe00bbcb1ce19ecdcee1e1e2e832bb6d3632
wangzhengkun <zhengkun@fit2cloud.com>
2021-02-01 15:24:50 +0800

    feat(password): 限制密码最低8位

--- ui/src/app/constant/pattern.ts
+++ ui/src/app/constant/pattern.ts
@@ -1,4 +1,4 @@
-export const PasswordPattern = '^(?=.*\\d)(?=.*[a-zA-Z])[\\da-zA-Z~!@#$%^&*]{6,30}$';
+export const PasswordPattern = '^(?=.*\\d)(?=.*[a-zA-Z])[\\da-zA-Z~!@#$%^&*]{8,30}$';
 export const NamePattern = '^[a-zA-Z\u4e00-\u9fa5]{1}[a-zA-Z0-9_\u4e00-\u9fa5]{0,30}$';
 export const EsIndexPattern = '^[a-zA-Z]{1}[a-zA-Z0-9]{0,30}$';
 export const EsIndexPatternHelper = '支持英文和数字';
