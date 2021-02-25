045d5b7ff0ada9abfea15734a51f0a4a4c30ae71
xiaohu <564134281@qq.com>
2021-02-25 18:07:54 +0800

    feat(setting): 初始化registry-setting页面

---
+++ ui/src/app/business/setting/registry-setting/registry-setting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registry-setting',
+  templateUrl: './registry-setting.component.html',
+  styleUrls: ['./registry-setting.component.css']
+})
+export class RegistrySettingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
