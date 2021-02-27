84d882094d2534cc4fac997bf4a78576da509bae
xiaohu <564134281@qq.com>
2021-02-27 16:51:50 +0800

    type(migrationRegistry): 删除多余的SQL描述符

--- migration/62_add_system_registry.up.sql
+++ migration/62_add_system_registry.up.sql
@@ -1,11 +1,12 @@
 CREATE TABLE IF NOT EXISTS `ko_system_registry`
 (
-    `created_at`        date                                             DEFAULT NULL,
-    `updated_at`        date                                             DEFAULT NULL,
-    `id`                varchar(254) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-    `registry_hostname` varchar(255) COLLATE utf8_bin                    DEFAULT NULL,
-    `registry_protocol` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
-    `architecture`      varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+    `created_at`        date         DEFAULT NULL,
+    `updated_at`        date         DEFAULT NULL,
+    `id`                varchar(64) NOT NULL,
+    `registry_hostname` varchar(255) DEFAULT NULL,
+    `registry_protocol` varchar(255) DEFAULT NULL,
+    `architecture`      varchar(255) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
-insert ignore into ko.ko_system_setting (`created_at`,`updated_at`,`id`,`key`,`value`,`tab`) values (date_add(now(), interval 8 HOUR), date_add(now(), interval 8 HOUR),UUID(),'arch_type','single','SYSTEM');
+insert ignore into ko.ko_system_setting (`created_at`, `updated_at`, `id`, `key`, `value`, `tab`)
+values (date_add(now(), interval 8 HOUR), date_add(now(), interval 8 HOUR), UUID(), 'arch_type', 'single', 'SYSTEM');
