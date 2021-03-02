0af51dcb49dfc95a0d996c537dd5aafad0ff04cf
xiaohu <564134281@qq.com>
2021-03-02 18:08:57 +0800

    feat(registry): 添加删除页面

--- ui/src/app/business/setting/registry-setting/registry-create/registry-create.component.ts
+++ ui/src/app/business/setting/registry-setting/registry-create/registry-create.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Registry, RegistryCreateRequest} from '../registry';
+import {NgForm} from '@angular/forms';
+import {BaseModelDirective} from '../../../../shared/class/BaseModelDirective';
+import {RegistryService} from '../registry.service';
+import {AlertLevels} from '../../../../layout/common-alert/alert';
+import {ModalAlertService} from '../../../../shared/common-component/modal-alert/modal-alert.service';
+import {CommonAlertService} from '../../../../layout/common-alert/common-alert.service';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-registry-create',
-  templateUrl: './registry-create.component.html',
-  styleUrls: ['./registry-create.component.css']
+    selector: 'app-registry-create',
+    templateUrl: './registry-create.component.html',
+    styleUrls: ['./registry-create.component.css']
 })
-export class RegistryCreateComponent implements OnInit {
+export class RegistryCreateComponent extends BaseModelDirective<Registry> implements OnInit {
+    opened = false;
+    isSubmitGoing = false;
 
-  constructor() { }
+    item: RegistryCreateRequest = new RegistryCreateRequest();
+    registries: Registry[] = [];
+    @ViewChild('registryForm') registryForm: NgForm;
+    @Output() created = new EventEmitter();
 
-  ngOnInit(): void {
-  }
+    constructor(private registryService: RegistryService, private modalAlertService: ModalAlertService,
+                private commonAlertService: CommonAlertService, private translateService: TranslateService) {
+        super(registryService);
+    }
 
+    ngOnInit(): void {
+    }
+
+    open() {
+        this.registryService.list().subscribe(data => {
+            this.registries = data.items;
+        });
+        this.opened = true;
+        this.item = new RegistryCreateRequest();
+        this.setDefaultValue();
+        // this.registryForm.resetForm();
+    }
+
+    onCancel() {
+        this.opened = false;
+    }
+
+    onSubmit() {
+        this.isSubmitGoing = true;
+        this.registryService.create(this.item).subscribe(data => {
+            this.opened = false;
+            this.isSubmitGoing = false;
+            this.created.emit();
+            this.onCancel();
+            this.commonAlertService.showAlert(this.translateService.instant('APP_ADD_SUCCESS'), AlertLevels.SUCCESS);
+        }, error => {
+            this.isSubmitGoing = false;
+            this.modalAlertService.showAlert(error.error.msg, AlertLevels.ERROR);
+        });
+    }
+
+    setDefaultValue() {
+        this.item.architecture = 'x86_64';
+        this.item.protocol = 'http';
+    }
 }
--- ui/src/app/business/setting/registry-setting/registry-delete/registry-delete.component.ts
+++ ui/src/app/business/setting/registry-setting/registry-delete/registry-delete.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Credential} from '../../credential/credential';
+import {AlertLevels} from '../../../../layout/common-alert/alert';
+import {Registry} from '../registry';
+import {CredentialService} from '../../credential/credential.service';
+import {ModalAlertService} from '../../../../shared/common-component/modal-alert/modal-alert.service';
+import {CommonAlertService} from '../../../../layout/common-alert/common-alert.service';
+import {TranslateService} from '@ngx-translate/core';
+import {RegistryService} from '../registry.service';
 
 @Component({
   selector: 'app-registry-delete',
@@ -6,10 +14,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registry-delete.component.css']
 })
 export class RegistryDeleteComponent implements OnInit {
-
-  constructor() { }
+  opened = false;
+  items: Registry[] = [];
+  @Output() deleted = new EventEmitter();
+  constructor(private service: RegistryService, private modalAlertService: ModalAlertService,
+              private commonAlertService: CommonAlertService, private translateService: TranslateService) {
+  }
 
   ngOnInit(): void {
+  }
+
+
+  open(items: Registry[]) {
+    this.items = items;
+    this.opened = true;
+  }
+
+  onCancel() {
+    this.opened = false;
+  }
+
+  onSubmit() {
+    this.service.batch('delete', this.items).subscribe(data => {
+      this.deleted.emit();
+      this.opened = false;
+      this.commonAlertService.showAlert(this.translateService.instant('APP_DELETE_SUCCESS'), AlertLevels.SUCCESS);
+    }, error => {
+      this.opened = false;
+      this.commonAlertService.showAlert(error.error.msg, AlertLevels.ERROR);
+    });
   }
 
 }
