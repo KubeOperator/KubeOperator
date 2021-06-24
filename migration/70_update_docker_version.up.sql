0bf027d8fafb238657da52f468e09a02a98c0d32
ssongliu <songlius11@163.com>
2021-06-24 19:28:52 +0800

    fix(manifest): 修改docker版本

---
+++ migration/70_update_docker_version.up.sql
@@ -0,0 +1,1 @@
+UPDATE ko_cluster_manifest SET core_vars='[{\"name\":\"kubernetes\",\"version\":\"v1.18.15\"},{\"name\":\"docker\",\"version\":\"19.03.15\"},{\"name\":\"etcd\",\"version\":\"v3.4.14\"},{\"name\":\"containerd\",\"version\":\"1.4.3\"}]' WHERE name='v1.18.15-ko1';
