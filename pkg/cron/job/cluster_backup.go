178150511aa9ff510ef9507f09b91a6519215f12
wangzhengkun <zhengkun@fit2cloud.com>
2021-07-06 10:35:39 +0800

    feat(backup): 修改自动备份逻辑

--- pkg/cron/job/cluster_backup.go
+++ pkg/cron/job/cluster_backup.go
@@ -28,35 +28,44 @@ func NewClusterBackup() *ClusterBackup {
 func (c *ClusterBackup) Run() {
 	logger.Log.Infof("start backup")
 	var wg sync.WaitGroup
-	sem := make(chan struct{}, 2) // 信号量
 	clusterBackupStrategies, _ := c.clusterBackupStrategyRepository.List()
 	for _, clusterBackupStrategy := range clusterBackupStrategies {
 		if clusterBackupStrategy.Status == "ENABLE" {
 			var backupFiles []model.ClusterBackupFile
-			db.DB.Find(&backupFiles)
-			if len(backupFiles) < clusterBackupStrategy.SaveNum {
-				if len(backupFiles) > 0 {
-					lastBackupFile := backupFiles[0]
-					backupDate := lastBackupFile.CreatedAt
-					now := time.Now()
-					sumD := now.Sub(backupDate)
-					day := int(math.Floor(sumD.Hours() / 24))
-					if day < clusterBackupStrategy.Cron {
-						continue
+			db.DB.Where("cluster_id = ?", clusterBackupStrategy.ClusterID).Order("created_at ASC").Find(&backupFiles)
+			if len(backupFiles) > 0 {
+				lastBackupFile := backupFiles[len(backupFiles)-1]
+				backupDate := lastBackupFile.CreatedAt
+				now := time.Now()
+				sumD := now.Sub(backupDate)
+				day := int(math.Floor(sumD.Hours() / 24))
+				if day < clusterBackupStrategy.Cron {
+					continue
+				}
+			}
+			var cluster model.Cluster
+			db.DB.Where("id = ?", clusterBackupStrategy.ClusterID).Find(&cluster)
+			if len(backupFiles) >= clusterBackupStrategy.SaveNum {
+				var deleteFileNum = len(backupFiles) - clusterBackupStrategy.SaveNum
+				for i := 0; i < deleteFileNum; i++ {
+					err := c.cLusterBackupFileService.Delete(backupFiles[i].Name)
+					if err != nil {
+						logger.Log.Errorf("delete cluster [%s] backup file error : %s", cluster.Name, err.Error())
 					}
 				}
+			}
+			db.DB.Where("cluster_id = ?", clusterBackupStrategy.ClusterID).Order("created_at ASC").Find(&backupFiles)
+			if len(backupFiles) < clusterBackupStrategy.SaveNum {
 				wg.Add(1)
 				go func() {
 					defer wg.Done()
-					sem <- struct{}{}
-					defer func() { <-sem }()
-					var cluster model.Cluster
-					db.DB.Where("id = ?", clusterBackupStrategy.ClusterID).Find(&cluster)
 					logger.Log.Infof("start backup cluster [%s]", cluster.Name)
 					if cluster.ID != "" {
 						err := c.cLusterBackupFileService.Backup(dto.ClusterBackupFileCreate{ClusterName: cluster.Name})
 						if err != nil {
 							logger.Log.Errorf("backup cluster error: %s", err.Error())
+						} else {
+							logger.Log.Infof("backup cluster [%s] success", cluster.Name)
 						}
 					}
 				}()
