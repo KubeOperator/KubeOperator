1071246e5409d2182029cc168fecedaba0c4edeb
ssongliu <songlius11@163.com>
2021-08-03 11:30:14 +0800

    fix(migration): node_port_range增加默认值

--- migration/89_alter_spec_nodeport.up.sql
+++ migration/89_alter_spec_nodeport.up.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `ko`.`ko_cluster_spec` ADD COLUMN `kube_service_node_port_range` VARCHAR(255) NULL AFTER `kube_proxy_mode`;
 ALTER TABLE `ko`.`ko_cluster_spec` ADD COLUMN `nodeport_address` VARCHAR(255) NULL AFTER `kube_proxy_mode`;
+UPDATE `ko_cluster_spec` SET `kube_service_node_port_range`='30000-32767'
