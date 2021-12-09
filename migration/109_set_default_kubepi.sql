fa84bb5717749708184f3c10cdac4d39200fba46
Aaron3S <chenyang@fit2cloud.com>
2021-12-09 18:51:02 +0800

    fix(kubepi): 修改kubepi默认信息

--- migration/109_set_default_kubepi.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO ko.ko_system_setting (created_at, updated_at, id, `key`, value, tab) VALUES (now(), now(), uuid(), 'KUBEPI_PASSWORD', 'Calong@2015', 'KUBEPI');
-INSERT INTO ko.ko_system_setting (created_at, updated_at, id, `key`, value, tab) VALUES (now(),now(), uuid(), 'KUBEPI_USERNAME', 'admin', 'KUBEPI');
