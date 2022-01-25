bb01c0af92a28fb37b1fa4005f5587c5af184765
ssongliu <songlius11@163.com>
2022-01-25 17:21:42 +0800

    fix(version): version 比较问题修复

--- pkg/util/version/version.go
+++ pkg/util/version/version.go
@@ -10,9 +10,19 @@ func IsNewerThan(v1, v2 string) bool {
 	v2 = strings.Replace(v2, "v", "", -1)
 	v1s := strings.Split(v1, ".")
 	v2s := strings.Split(v2, ".")
+	if len(v1s) != 3 || len(v2s) != 3 {
+		return false
+	}
+
 	for i := 0; i < 3; i++ {
-		v1si, _ := strconv.Atoi(v1s[i])
-		v2si, _ := strconv.Atoi(v2s[i])
+		v1si, err := strconv.Atoi(v1s[i])
+		if err != nil {
+			return false
+		}
+		v2si, err := strconv.Atoi(v2s[i])
+		if err != nil {
+			return false
+		}
 		if v1si > v2si {
 			return true
 		}
