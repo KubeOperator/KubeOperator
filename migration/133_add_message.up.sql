567101e9024578ae0808301c7adfa2d73e423f0a
wangzhengkun <zhengkun@fit2cloud.com>
2022-07-15 10:54:54 +0800

    feat: 数据库文件修改

--- migration/133_add_message.up.sql
+++ migration/133_add_message.up.sql
@@ -43,6 +43,39 @@ INSERT INTO `ko_user_setting` (`id`, `user_id`, `msg`, `created_at`, `updated_at
 SELECT UUID(),id,'{\"dingTalk\":{\"account\":\"\",\"receive\":\"DISABLE\"},\"email\":{\"account\":\"\",\"receive\":\"DISABLE\"},\"workWeiXin\":{\"account\":\"\",\"receive\":\"DISABLE\"},\"local\":{\"account\":\"\",\"receive\":\"ENABLE\"}}',date_add(now(), interval 8 HOUR),  date_add(now(), interval 8 HOUR) FROM ko_user;
 
 
+CREATE TABLE `ko_msg_subscribe_user` (
+  `subscribe_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `user_id` varchar(64) NOT NULL,
+  `id` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `ko_user_msg` (
+  `id` varchar(64) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `receive` varchar(255) DEFAULT NULL,
+  `user_id` varchar(64) DEFAULT NULL,
+  `msg_id` varchar(64) DEFAULT NULL,
+  `send_type` varchar(64) DEFAULT NULL,
+  `send_status` varchar(64) DEFAULT NULL,
+  `read_status` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `ko_msg` (
+  `id` varchar(64) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `content` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `level` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `resource_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `resource_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `resource_tyoe` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 
 
